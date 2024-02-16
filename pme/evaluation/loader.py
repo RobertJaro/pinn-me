add test set generation, add image scaling, fix voigt profile

pme/evaluation/loader.py
@@ -38,13 +38,6 @@ class PINNMEOutput:
         parameters = {key: np.concatenate(value).reshape(*coords_shape[:-1], -1)
                       for key, value in parameters.items()}
 
-        # adjust angles and b_field
-        # flip_mask = np.zeros_like(parameters['b_field'])
-        # flip_mask[np.sign(parameters['b_field']) == -1] = np.pi / 2
-        parameters['theta'] = (parameters['theta']) % np.pi
-        parameters['chi'] = parameters['chi'] % np.pi
-        parameters['b_field'] = parameters['b_field']
-
         return parameters
 
     def load_cube(self):
@@ -66,3 +59,16 @@ class PINNMEOutput:
         coords = np.stack(coords, axis=-1)
 
         return self.load(coords)
+
+
+def to_cartesian(b_field, theta, chi):
+    b_x = b_field * np.sin(theta) * np.cos(chi)
+    b_y = b_field * np.sin(theta) * np.sin(chi)
+    b_z = b_field * np.cos(theta)
+    return np.stack([b_x, b_y, b_z], axis=-1)
+
+def to_spherical(b):
+    b_field = np.linalg.norm(b, axis=-1)
+    theta = np.arccos(b[..., 2] / b_field)
+    chi = np.arctan2(b[..., 1], b[..., 0])
+    return b_field, theta, chi

pme/inversion.py
@@ -50,7 +50,7 @@ if wandb_id is not None:
 
 data_module = TestDataModule(**args.data)
 
-me_module = MEModule(data_module.cube_shape, data_module.lambda_grid, **args.model, **args.training)
+me_module = MEModule(data_module.cube_shape, data_module.lambda_grid, data_module.value_range, **args.model, **args.training)
 
 config = {'data': args.data, 'model': args.model, 'training': args.training}
 checkpoint_callback = ModelCheckpoint(dirpath=base_path,
@@ -59,7 +59,7 @@ checkpoint_callback = ModelCheckpoint(dirpath=base_path,
                                       save_last=True)
 
 # save callback
-save_path = os.path.join(base_path, 'inversion.pinnme')
+save_path = os.path.join(base_path, 'inversion.pme')
 def save(*args, **kwargs):
     torch.save({
                 'parameter_model': me_module.parameter_model,

pme/train/generic_function.py
@@ -273,7 +273,7 @@ class Voigt(nn.Module):
         from See Humlicek (1982) JQSRT 27, 437
         '''
         z = (x + 1j * gamma) / (2 ** 0.5 * sigma)
-        v = self.faddeeva(z).real / (sigma * (2 * np.pi) ** 0.5)
+        v = self.faddeeva(z).real / (sigma * (2 * np.pi) ** 0.5) * 1.414
         return v
 
 def faraday_voigt(nu, sigma, gamma, mu):
@@ -313,9 +313,9 @@ if __name__ == '__main__':
     test_mu = np.ones_like(test_nu) * t_mu
     test_sigma = np.ones_like(test_nu)
 
-    test_profile = faraday_voigt(test_nu, test_sigma, test_gamma, test_mu)
+    test_profile = voigt(test_nu, test_sigma, test_gamma, test_mu)
 
-    f = FaradayVoigt(16)
+    f = Voigt()
 
     test_nu_t = torch.tensor(test_nu, dtype=torch.float32)[:, None].to(device)
     test_gamma_t = torch.tensor(test_gamma, dtype=torch.float32)[:, None].to(device)

pme/train/me_module.py
@@ -2,18 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import wandb
+from astropy.visualization import ImageNormalize, AsinhStretch
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from pytorch_lightning import LightningModule
 from torch import nn
 from torch.optim.lr_scheduler import ExponentialLR
 
-from pme.model import MEModel
+from pme.model import MEModel, NormalizationModule
 from pme.train.me_equations import MEAtmosphere
 
 
 class MEModule(LightningModule):
 
-    def __init__(self, cube_shape, lambda_grid, psf_config={'type': None}, dim=256,
+    def __init__(self, cube_shape, lambda_grid, value_range, psf_config={'type': None}, dim=256,
                  lr_params={"start": 5e-4, "end": 5e-5, "iterations": 1e5},
                  encoding="positional", plot_profiles=False, **kwargs):
         super().__init__()
@@ -60,8 +61,8 @@ class MEModule(LightningModule):
         self.lr_params = lr_params
         #
         self.validation_outputs = {}
-        weight = torch.tensor([1., 1e2, 1e2, 1e1], dtype=torch.float32).reshape(1, 4, 1)
-        self.weight = weight
+        self.normalization = NormalizationModule(value_range)
+        self.loss_function = nn.MSELoss(reduction='none')
 
     def configure_optimizers(self):
         parameters = list(self.parameter_model.parameters())
@@ -116,11 +117,18 @@ class MEModule(LightningModule):
         # stokes_pred = torch.arcsinh(stokes_pred / 1e-3) / np.arcsinh(1 / 1e-3)
         # stokes_true = torch.arcsinh(stokes_true / 1e-3) / np.arcsinh(1 / 1e-3)
 
-        weight = self.weight.to(stokes_pred.device)
-        loss = ((stokes_pred - stokes_true) * weight).pow(2).sum(-1)
-        loss = torch.mean(loss)
+        # weight = self.weight.to(stokes_pred.device)
+        stokes_true = self.normalization(stokes_true)
+        stokes_pred = self.normalization(stokes_pred)
 
-        return {"loss": loss}
+        loss = self.loss_function(stokes_pred, stokes_true)
+        loss = loss.sum(dim=-1)
+        total_loss = loss.mean()
+        I_loss, Q_loss, U_loss, V_loss = loss.mean(dim=0)
+
+        return {"loss": total_loss,
+                "I_loss": I_loss, "Q_loss": Q_loss,
+                "U_loss": U_loss, "V_loss": V_loss}
 
     def convolve_psf(self, I, Q, U, V, coords):
         # filter_outside = (coords[..., 0] < 0) | (coords[..., 0] > 1) | (coords[..., 1] < 0) | (coords[..., 1] > 1)
@@ -191,7 +199,6 @@ class MEModule(LightningModule):
             outputs[k] = torch.cat([o[k] for o in outputs_list], dim=0)
 
         self.log("valid", {"diff": outputs['diff'].mean()})
-
         for k in ['b_field', 'theta', 'chi', 'vmac', 'damping', 'b0', 'b1', 'mu', 'vdop', 'kl']:
             field = outputs[k].reshape(*self.cube_shape[:2]).cpu().numpy()
             plot_settings = {}
@@ -234,15 +241,16 @@ class MEModule(LightningModule):
         integerated_stokes_true = np.abs(stokes_true).sum(-1)
         integerated_stokes_pred = np.abs(stokes_pred).sum(-1)
         fig, ax = plt.subplots(2, 4, figsize=(16, 8), dpi=100)
-        for i, label in enumerate(['I', 'Q', 'U', 'V']):
+        for i, label in enumerate(['I', 'Q/I', 'U/I', 'V/I']):
             v_min = integerated_stokes_true[:, :, i].min()
             v_max = integerated_stokes_true[:, :, i].max()
-            im = ax[0, i].imshow(integerated_stokes_true[:, :, i], vmin=v_min, vmax=v_max)
+            norm = ImageNormalize(stretch=AsinhStretch(1e-1), vmin=v_min, vmax=v_max)
+            im = ax[0, i].imshow(integerated_stokes_true[:, :, i], norm = norm)
             ax[0, i].set_title(f"true - {label}")
             divider = make_axes_locatable(ax[0, i])
             cax = divider.append_axes("right", size="5%", pad=0.05)
             fig.colorbar(im, cax=cax)
-            im = ax[1, i].imshow(integerated_stokes_pred[:, :, i], vmin=v_min, vmax=v_max)
+            im = ax[1, i].imshow(integerated_stokes_pred[:, :, i], norm=norm)
             ax[1, i].set_title(f"pred - {label}")
             divider = make_axes_locatable(ax[1, i])
             cax = divider.append_axes("right", size="5%", pad=0.05)