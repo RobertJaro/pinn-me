fix grid scaling, unify lambda grid, fix lambda grid, update hinode data loader, remove I scaling,  update logging plots, add encodings

pme/data/test_set.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as pl
 import numpy as np
 import torch
 from astropy import units as u
+from matplotlib.colors import LogNorm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from pme.train.me_atmosphere import MEAtmosphere
@@ -19,23 +20,37 @@ def plot_stokes(profile, save_path):
     Input:
         -- atmos, ndarray [4, num_Intensity]
     """
+    profile = np.abs(profile)
+
     fig, axs = pl.subplots(1, 4, figsize=(16, 4))
 
     ax = axs[0]
-    ax.imshow(profile[..., 0, :].sum(axis=-1))
+    im = ax.imshow(profile[..., 0, :].sum(axis=-1), norm=LogNorm())
     ax.set_title("I")
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    pl.colorbar(im, cax=cax)
 
     ax = axs[1]
-    ax.imshow(profile[..., 1, :].sum(axis=-1))
+    im = ax.imshow(profile[..., 1, :].sum(axis=-1), norm=LogNorm())
     ax.set_title("Q")
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    pl.colorbar(im, cax=cax)
 
     ax = axs[2]
-    ax.imshow(profile[..., 2, :].sum(axis=-1))
+    im = ax.imshow(profile[..., 2, :].sum(axis=-1), norm=LogNorm())
     ax.set_title("U")
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    pl.colorbar(im, cax=cax)
 
     ax = axs[3]
-    ax.imshow(profile[..., 3, :].sum(axis=-1))
+    im = ax.imshow(profile[..., 3, :].sum(axis=-1), norm=LogNorm())
     ax.set_title("V")
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    pl.colorbar(im, cax=cax)
 
     pl.tight_layout()
     pl.savefig(save_path, dpi=150)
@@ -53,70 +68,70 @@ def plot_parameters(parameters, save_path):
     fig, axs = pl.subplots(2, 5, figsize=(16, 4))
 
     ax = axs[0, 0]
-    im = ax.imshow(parameters['b_field'], cmap='RdBu_r')
+    im = ax.imshow(parameters['b_field'].T, cmap='jet', vmin=0)
     ax.set_title("B")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[0, 1]
-    im = ax.imshow(parameters['theta'], cmap='RdBu_r')
+    im = ax.imshow(parameters['theta'].T, cmap='RdBu_r')
     ax.set_title("Theta")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[0, 2]
-    im = ax.imshow(parameters['chi'], cmap='twilight')
+    im = ax.imshow(parameters['chi'].T, cmap='twilight')
     ax.set_title("Chi")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[0, 3]
-    im = ax.imshow(parameters['b0'])
+    im = ax.imshow(parameters['b0'].T)
     ax.set_title("B0")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[0, 4]
-    im = ax.imshow(parameters['b1'])
+    im = ax.imshow(parameters['b1'].T)
     ax.set_title("B1")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[1, 0]
-    im = ax.imshow(parameters['vmac'])
+    im = ax.imshow(parameters['vmac'].T)
     ax.set_title("Vmac")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[1, 1]
-    im = ax.imshow(parameters['damping'])
+    im = ax.imshow(parameters['damping'].T)
     ax.set_title("Damping")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[1, 2]
-    im = ax.imshow(parameters['mu'])
+    im = ax.imshow(parameters['mu'].T)
     ax.set_title("Mu")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[1, 3]
-    im = ax.imshow(parameters['vdop'])
+    im = ax.imshow(parameters['vdop'].T)
     ax.set_title("Vdop")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     pl.colorbar(im, cax=cax)
 
     ax = axs[1, 4]
-    im = ax.imshow(parameters['kl'])
+    im = ax.imshow(parameters['kl'].T)
     ax.set_title("Kl")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
@@ -147,17 +162,15 @@ class TestSetGenerator():
                  j_up=1.0, j_low=0.0, g_up=2.49, g_low=0,
                  lambda_start=6301.989128432432 * u.AA, lambda_step=0.021743135134784097 * u.AA, n_lambda=56,
                  nx=400, ny=400,
-                 b_field_0=1000.0, vmac=2.0 * 1e3, damping=0.2, b0=0.8, b1=0.2, mu=1.0, vdop=2.0 * 1e3, kl=1.0):
+                 b_field_0=2000.0, vmac=2.0 * 1e3, damping=0.2, b0=0.8, b1=0.2, mu=1.0, vdop=2.0 * 1e3, kl=25.0):
         self.lambda0 = lambda0
         self.jUp = j_up
         self.jLow = j_low
         self.gUp = g_up
         self.gLow = g_low
 
-        lambda_end = (lambda_start + lambda_step * (-1 + n_lambda))
-        self.lambda_grid = np.linspace(-.5 * (lambda_end - lambda_start),
-                                       .5 * (lambda_end - lambda_start),
-                                       num=n_lambda)
+        lambda_range = (n_lambda - 1) * lambda_step
+        self.lambda_grid = np.linspace(-0.5 * lambda_range, 0.5 * lambda_range, n_lambda)
 
         # Inputs for the inversion
         self.b_field_0 = b_field_0

pme/model.py
@@ -28,8 +28,8 @@ class PeriodicBoundary(nn.Module):
         super().__init__()
 
     def forward(self, coord):
-        scaled_x = coord[..., 0:1] * torch.pi # - pi to pi
-        scaled_y = coord[..., 1:2] * torch.pi # - pi to pi
+        scaled_x = coord[..., 0:1] * torch.pi  # - pi to pi
+        scaled_y = coord[..., 1:2] * torch.pi  # - pi to pi
         encoded_coord = torch.cat([
             torch.sin(scaled_x), torch.cos(scaled_x),
             torch.sin(scaled_y), torch.cos(scaled_y),
@@ -41,18 +41,21 @@ class MEModel(nn.Module):
 
     def __init__(self, in_coords, dim, encoding='positional', num_layers=8):
         super().__init__()
-
         # encoding layer
         if encoding == "periodic":
             posenc = PeriodicBoundary()
             d_in = nn.Linear(in_coords + 2, dim)
             self.d_in = nn.Sequential(posenc, d_in)
         elif encoding == "positional":
-            posenc = PositionalEncoding(n_freqs=20, d_input=in_coords)
+            posenc = PositionalEncoding(num_freqs=20, d_input=in_coords)
             d_in = nn.Linear(posenc.d_output, dim)
             self.d_in = nn.Sequential(posenc, d_in)
         elif encoding == "learned_positional":
-            posenc = LearnedPositionalEncoding(num_freqs=20, in_features=in_coords)
+            posenc = LearnedPositionalEncoding(num_freqs=20, d_input=in_coords)
+            d_in = nn.Linear(posenc.d_output, dim)
+            self.d_in = nn.Sequential(posenc, d_in)
+        elif encoding == "gaussian_positional":
+            posenc = GaussianPositionalEncoding(num_freqs=20, d_input=in_coords)
             d_in = nn.Linear(posenc.d_output, dim)
             self.d_in = nn.Sequential(posenc, d_in)
         elif encoding == "linear":
@@ -82,6 +85,83 @@ class MEModel(nn.Module):
         params = self.d_out(x)
         #
         b_field = params[..., 0:1] * 1000
+        theta = params[..., 1:2]# * torch.pi
+        chi = params[..., 2:3]# * torch.pi
+        vmac = torch.sigmoid(params[..., 3:4]) * 20e3
+        damping = torch.sigmoid(params[..., 4:5]) * 1
+        b0 = torch.sigmoid(params[..., 5:6])
+        b1 = torch.sigmoid(params[..., 6:7])
+        vdop = params[..., 7:8] * 1e4
+        kl = torch.sigmoid(params[..., 8:9]) * 100
+        #
+        output = {
+            "b_field": b_field,
+            "theta": theta,
+            "chi": chi,
+            "vmac": vmac,
+            "damping": damping,
+            "b0": b0,
+            "b1": b1,
+            "vdop": vdop,
+            "kl": kl,
+        }
+
+        return output
+
+
+class ParameterModel(nn.Module):
+
+    def __init__(self, in_coords, dim, num_layers=8, encoding='positional'):
+        super().__init__()
+        # encoding layer
+        if encoding == "periodic":
+            posenc = PeriodicBoundary()
+            d_in = nn.Linear(in_coords + 2, dim)
+            self.d_in = nn.Sequential(posenc, d_in)
+        elif encoding == "positional":
+            posenc = PositionalEncoding(num_freqs=20, d_input=in_coords)
+            d_in = nn.Linear(posenc.d_output, dim)
+            self.d_in = nn.Sequential(posenc, d_in)
+        elif encoding == "learned_positional":
+            posenc = LearnedPositionalEncoding(num_freqs=20, d_input=in_coords)
+            d_in = nn.Linear(posenc.d_output, dim)
+            self.d_in = nn.Sequential(posenc, d_in)
+        elif encoding == "gaussian_positional":
+            posenc = GaussianPositionalEncoding(num_freqs=20, d_input=in_coords)
+            d_in = nn.Linear(posenc.d_output, dim)
+            self.d_in = nn.Sequential(posenc, d_in)
+        elif encoding == "linear":
+            self.d_in = nn.Linear(in_coords, dim)
+        else:
+            raise ValueError(f"Unknown encoding: {encoding}")
+
+        # hidden layers
+        lin = [nn.Linear(dim, dim) for _ in range(num_layers)]
+        self.linear_layers = nn.ModuleList(lin)
+        # output layer
+        self.d_out = nn.Linear(dim, 1)
+        # activation functions
+        self.in_activation = Swish()
+        self.activations = nn.ModuleList([Swish() for _ in range(num_layers)])
+
+    def forward(self, x):
+        x = self.in_activation(self.d_in(x))
+        for l, a in zip(self.linear_layers, self.activations):
+            x = a(l(x))
+        return self.d_out(x)
+
+
+class ParametersModel(nn.Module):
+
+    def __init__(self, in_coords, dim, encoding='positional', num_layers=8):
+        super().__init__()
+        parameter_models = [ParameterModel(in_coords, dim, encoding=encoding, num_layers=num_layers) for _ in range(9)]
+        self.parameter_models = nn.ModuleList(parameter_models)
+
+    def forward(self, x):
+        params = torch.cat([m(x) for m in self.parameter_models], -1)
+        #
+        b_field = params[..., 0:1] * 1e3
         theta = params[..., 1:2] * torch.pi
         chi = params[..., 2:3] * torch.pi
         vmac = torch.sigmoid(params[..., 3:4]) * 20e3
@@ -109,11 +189,11 @@ class MEModel(nn.Module):
 # revert this
 class LearnedPositionalEncoding(nn.Module):
 
-    def __init__(self, num_freqs, in_features):
+    def __init__(self, num_freqs, d_input):
         super().__init__()
-        frequencies = torch.randn(num_freqs, in_features)
+        frequencies = torch.randn(num_freqs, d_input)
         self.frequencies = nn.Parameter(frequencies[None], requires_grad=True)
-        self.d_output = in_features * (num_freqs * 2 + 1)
+        self.d_output = d_input * (num_freqs * 2 + 1)
 
     def forward(self, x):
         encoded = x[:, None, :] * torch.pi * 2 ** self.frequencies
@@ -122,25 +202,39 @@ class LearnedPositionalEncoding(nn.Module):
         return encoded
 
 
+class GaussianPositionalEncoding(nn.Module):
+
+    def __init__(self, num_freqs, d_input):
+        super().__init__()
+        frequencies = torch.randn(num_freqs, d_input)
+        self.frequencies = nn.Parameter(frequencies[None], requires_grad=False)
+        self.d_output = d_input * (num_freqs * 2 + 1)
+
+    def forward(self, x):
+        encoded = x[:, None, :] * self.frequencies
+        encoded = encoded.reshape(x.shape[0], -1)
+        encoded = torch.cat([x, torch.sin(encoded), torch.cos(encoded)], -1)
+        return encoded
+
 
 class PositionalEncoding(nn.Module):
     r"""
     Sine-cosine positional encoder for input points.
     """
 
-    def __init__(self, d_input: int, n_freqs: int, scale_factor: float = 2., log_space: bool = True):
+    def __init__(self, d_input: int, num_freqs: int, scale_factor: float = 2., log_space: bool = True):
         """
 
         Parameters
         ----------
         d_input: number of input dimensions
-        n_freqs: number of frequencies used for encoding
+        num_freqs: number of frequencies used for encoding
         scale_factor: factor to adjust box size limit of 2pi (default 2; 4pi)
         log_space: use frequencies in powers of 2
         """
         super().__init__()
         self.d_input = d_input
-        self.n_freqs = n_freqs
+        self.n_freqs = num_freqs
         self.log_space = log_space
         self.d_output = d_input * (1 + 2 * self.n_freqs)
 
@@ -180,13 +274,9 @@ class NormalizationModule(nn.Module):
     def __init__(self, value_range):
         super().__init__()
         self.register_buffer("value_range", torch.tensor(value_range, dtype=torch.float32)[None, :, None, :])
-        self.register_buffer("stretch", torch.tensor(np.arcsinh(1e1), dtype=torch.float32))
+        self.register_buffer("stretch", torch.tensor(np.arcsinh(1e2), dtype=torch.float32))
 
     def forward(self, stokes):
-        I = stokes[..., 0:1, :]
-        normalized_QUV = stokes[..., 1:, :] / (I + 1e-8)
-        stokes = torch.cat([I, normalized_QUV], dim=-2)
-
-        stokes = (stokes - self.value_range[..., 0]) / (self.value_range[..., 1] - self.value_range[..., 0])
-        stokes = torch.asinh(stokes * 1e1) / self.stretch
+        stokes = stokes / self.value_range[..., 1] # normalize by max value (I = [0, 1]; QUV = [-1, 1])
+        stokes = torch.asinh(stokes * 1e2) / self.stretch
         return stokes

pme/train/data_loader.py
@@ -49,15 +49,15 @@ def apply_along_space(f, np_array, axes, progress_bar=True):
 class GenericDataModule(LightningDataModule):
 
     def __init__(self, stokes_vector, mu, times, lambda_config,
-                 seconds_per_dt=3600, pixel_per_ds=1e-2,
+                 seconds_per_dt=3600, pixel_per_ds=1e2,
                  batch_size=4096, num_workers=None):
         super().__init__()
-
         assert stokes_vector.shape[0] == len(times), \
             f'Times need to match Stokes vector: {stokes_vector.shape[0]} != {len(times)}'
 
         # train parameters
-        self.batch_size = batch_size
+        n_gpus = torch.cuda.device_count() if torch.cuda.is_available() else 1
+        self.batch_size = batch_size * n_gpus
         self.num_workers = num_workers if num_workers is not None else os.cpu_count()
 
         #
@@ -67,14 +67,9 @@ class GenericDataModule(LightningDataModule):
         self.seconds_per_dt = seconds_per_dt
         self.pixel_per_ds = pixel_per_ds
 
-        # lambda grid
-        lambda_start = lambda_config['start'] * u.AA
-        lambda_step = lambda_config['step'] * u.AA
-        lambda_end = (lambda_start + lambda_step * (lambda_config['n'] - 1))
         # centered at lambda0
-        self.lambda_grid = np.linspace(-.5 * (lambda_end - lambda_start), .5 * (lambda_end - lambda_start),
-                                       num=lambda_config['n'])
-        self.lambda_config = {'lambda0': lambda_config['0'] * u.AA,
+        self.lambda_grid = lambda_config['lambda_grid']
+        self.lambda_config = {'lambda0': lambda_config['0'],
                               'j_up': lambda_config['j_up'], 'j_low': lambda_config['j_low'],
                               'g_up': lambda_config['g_up'], 'g_low': lambda_config['g_low'],
                               'lambda_grid': self.lambda_grid}
@@ -98,11 +93,9 @@ class GenericDataModule(LightningDataModule):
                            [coordinates[..., 1].min(), coordinates[..., 1].max()],
                            [coordinates[..., 2].min(), coordinates[..., 2].max()]]
 
-        normalized_stokes_vector = np.copy(stokes_vector)
-        normalized_stokes_vector[:, :, :, 1:] /= normalized_stokes_vector[:, :, :, 0:1]
-        self.value_range = np.stack([normalized_stokes_vector.min((0, 1, 2, -1)),
-                                     normalized_stokes_vector.max((0, 1, 2, -1))], -1)
-        print('VALUE RANGE: ', self.value_range)
+        self.value_range = np.stack([stokes_vector.min((0, 1, 2, -1)),
+                                        stokes_vector.max((0, 1, 2, -1))], -1)
+        print('VALUE RANGE', self.value_range)
 
         # plot coordinates
         ref_time = stokes_vector.shape[0] // 2
@@ -125,7 +118,8 @@ class GenericDataModule(LightningDataModule):
         for l in range(0, stokes_vector.shape[-1], stokes_vector.shape[-1] // 10):
             fig, axs = plt.subplots(1, 4, figsize=(9, 3), dpi=100)
             for i, label in enumerate(['I', 'Q', 'U', 'V']):
-                im = axs[i].imshow(stokes_vector[ref_time, :, :, i, l], vmin=-stokes_min_max[i], vmax=stokes_min_max[i])
+                im = axs[i].imshow(stokes_vector[ref_time, :, :, i, l].T, vmin=-stokes_min_max[i],
+                                   vmax=stokes_min_max[i])
                 axs[i].set_title(label)
                 fig.colorbar(im, ax=axs[i])
             fig.suptitle(f'lambda: {self.lambda_grid[l]:.2f}')
@@ -137,7 +131,7 @@ class GenericDataModule(LightningDataModule):
         integrated_stokes_vector = np.abs(stokes_vector).sum(-1)
         fig, ax = plt.subplots(1, 4, figsize=(16, 8), dpi=100)
         for i, label in enumerate(['I', 'Q', 'U', 'V']):
-            im = ax[i].imshow(integrated_stokes_vector[ref_time, :, :, i])
+            im = ax[i].imshow(integrated_stokes_vector[ref_time, :, :, i].T)
             ax[i].set_title(label)
             fig.colorbar(im, ax=ax[i])
         fig.tight_layout()
@@ -191,7 +185,15 @@ class GenericDataModule(LightningDataModule):
 class TestDataModule(GenericDataModule):
 
     def __init__(self, files, psf=None, noise=None, **kwargs):
-        lambda_config = {'0': 6302.4931, 'start': 6301.989128432432, 'step': 0.021743135134784097, 'n': 56,
+        lambda_center = 6302.4931
+        lambda_step = 0.021743135134784097
+        n_lambda = 56
+
+        # lambda_grid = np.array([lambda_start + i * lambda_step for i in range(n_lambda)])
+        lambda_range = (n_lambda - 1) * lambda_step
+        lambda_grid = np.linspace(-0.5 * lambda_range, 0.5 * lambda_range, n_lambda)
+
+        lambda_config = {'0': lambda_center * u.AA, 'lambda_grid': lambda_grid * u.AA,
                          'j_up': 1.0, 'j_low': 0.0, 'g_up': 2.49, 'g_low': 0}
         files = [files] if not isinstance(files, list) else files
         files = [sorted(glob.glob(f)) for f in files]  # load wildcards
@@ -208,7 +210,7 @@ class TestDataModule(GenericDataModule):
 
         mu = np.ones((*stokes_vector.shape[:3], 1), dtype=np.float32)
 
-        super().__init__(stokes_vector, mu, times, lambda_config)
+        super().__init__(stokes_vector, mu, times, lambda_config, **kwargs)
 
 
 class HinodeDataModule(GenericDataModule):
@@ -217,25 +219,39 @@ class HinodeDataModule(GenericDataModule):
         files = [files] if not isinstance(files, list) else files
         files = [sorted(glob.glob(f)) for f in files]  # load wildcards
 
-        stokes_vector = [np.stack([fits.getdata(f) for f in file_list], 1) for file_list in files]
+        stokes_vector = [np.stack([fits.getdata(f) for f in file_list], 0) for file_list in files]
 
         stokes_vector = np.stack(stokes_vector, 0, dtype=np.float32)
-        stokes_vector = np.moveaxis(stokes_vector, [1, 2, 3], [3, 1, 2])
+
+        stokes_vector = np.moveaxis(stokes_vector, [1, 2, 3], [1, 3, 2])
 
         times = [parse(getheader(f[0])['DATE_OBS']) for f in files]
 
-        n_lambda = stokes_vector.shape[-1]
-
         if lambda_config is None:
-            # lambda_config = {'0': 6302.4931, 'start': 6301.989128432432, 'step': 0.021743135134784097, 'n': 56,
-            #                  'j_up': 1.0, 'j_low': 0.0, 'g_up': 2.49, 'g_low': 0}
-            lambda_config = {'0': 6302.4931, 'start': 6301.989128432432, 'step': 0.021743135134784097, 'n': n_lambda,
-                             'j_up': 1.0, 'j_low': 0.0, 'g_up': 2.49, 'g_low': 0}
+            header = getheader(files[0][0])
+            lambda_step = -header["CDELT1"]
+            lambda_center = header["CRVAL1"]
+            pixel_center = header["CRPIX1"]
+            offset = 6302.4931 - lambda_center
+            n_lambda = stokes_vector.shape[-1]
 
+            pixel_range = np.arange(n_lambda)
+            pixel_range = pixel_range - pixel_center
+            lambda_range = pixel_range * lambda_step
+            lambda_grid = lambda_range - offset
+
+            stokes_vector = stokes_vector[..., -56:]
+            lambda_grid = lambda_grid[-56:]
+
+            max_I = stokes_vector[..., 0, :].max()
+            stokes_vector /= max_I * 1.1
+
+            lambda_config = {'0': lambda_center * u.AA, 'lambda_grid': lambda_grid * u.AA,
+                             'j_up': 1.0, 'j_low': 0.0, 'g_up': 2.49, 'g_low': 0}
 
         mu = np.ones((*stokes_vector.shape[:3], 1), dtype=np.float32)
 
-        super().__init__(stokes_vector, mu, times, lambda_config)
+        super().__init__(stokes_vector, mu, times, lambda_config, pixel_per_ds=512, **kwargs)
 
 
 def load_test_stokes_profiles(file, psf_file=None, noise=None):

pme/train/me_atmosphere.py
@@ -156,7 +156,7 @@ class MEAtmosphere(nn.Module):
         return V
 
     def nu_m(self, b_field, d_lambda, **kwargs):
-        dlambda_B = 1e-13 * 4.6686e10 * (self.lambda0 ** 2) * b_field
+        dlambda_B = 4.6686e-3 * (self.lambda0 ** 2) * b_field
         return dlambda_B / d_lambda
 
     def lambda_dop(self, vdop, d_lambda, **kwargs):

pme/train/me_module.py
@@ -9,32 +9,32 @@ from pytorch_lightning import LightningModule
 from torch import nn
 from torch.optim.lr_scheduler import ExponentialLR
 
-from pme.model import MEModel, NormalizationModule
+from pme.evaluation.loader import to_spherical, to_cartesian
+from pme.model import MEModel, NormalizationModule, ParametersModel
 from pme.train.me_atmosphere import MEAtmosphere
 
 
 class MEModule(LightningModule):
 
     def __init__(self, cube_shape, lambda_config, value_range, pixel_per_ds, psf_config={'type': None},
-                 dim=256, lr_params={"start": 5e-4, "end": 5e-5, "iterations": 1e5}, lambda_stokes=[0.1, 1, 1, 1],
-                 encoding="positional", plot_profiles=False, **kwargs):
+                 dim=256, lr_params={"start": 5e-4, "end": 5e-5, "iterations": 1e5}, lambda_stokes=[1, 1, 1, 1],
+                 encoding="positional", **kwargs):
         super().__init__()
-
-        self.plot_profiles = plot_profiles
 
         self.cube_shape = cube_shape
 
         # init model
         self.parameter_model = MEModel(3, dim, encoding=encoding)
 
-        if psf_config['type'] is None:
+        psf_type = psf_config.pop('type')
+        if psf_type is None:
             self.psf = NoPSF()
-        elif psf_config['type'] == 'load':
-            self.psf = LoadPSF(psf_config['file'])
-        elif psf_config['type'] == 'learn':
-            self.psf = PSF(*psf_config['shape'])
+        elif psf_type == 'load':
+            self.psf = LoadPSF(**psf_config)
+        elif psf_type == 'learn':
+            self.psf = PSF(**psf_config)
         else:
-            raise ValueError(f"Invalid PSF type: {psf_config['type']}")
+            raise ValueError(f"Invalid PSF type: {psf_type}")
 
         # initialize PSF coordinates
         psf_shape = self.psf.psf_shape
@@ -83,6 +83,9 @@ class MEModule(LightningModule):
         coords_shape = coords.shape
         output = self.parameter_model(coords.reshape(-1, 3))
 
+        # expand mu for PSF (TODO adapt mu for PSF sampling)
+        mu = mu[:, None, None, :].repeat(1, *self.coords_psf.shape[1:3], 1).reshape(-1, 1)
+
         I, Q, U, V = self.forward_model(**output, mu=mu)
         I = I.reshape(*coords_shape[:-1], -1)
         Q = Q.reshape(*coords_shape[:-1], -1)
@@ -129,7 +132,6 @@ class MEModule(LightningModule):
         # log results to WANDB
         self.log("train", {k: v.mean() for k, v in outputs.items()})
 
-    @torch.enable_grad()
     def validation_step(self, batch, batch_nb):
         coords, mu, stokes_true = batch
 
@@ -137,6 +139,9 @@ class MEModule(LightningModule):
 
         coords_shape = coords.shape
         output = self.parameter_model(coords.reshape(-1, 3))
+
+        # expand mu for PSF (TODO adapt mu for PSF sampling)
+        mu = mu[:, None, None, :].repeat(1, *self.coords_psf.shape[1:3], 1).reshape(-1, 1)
 
         I, Q, U, V = self.forward_model(**output, mu=mu)
 
@@ -175,26 +180,73 @@ class MEModule(LightningModule):
         self.log("valid", {"diff": outputs['diff'].mean(),
                            'I_diff': I_diff, 'Q_diff': Q_diff, 'U_diff': U_diff, 'V_diff': V_diff})
 
-        parameters = self.plot_parameters(outputs)
+        parameters = {}
+        for k in ['b_field', 'theta', 'chi', 'vmac', 'damping', 'b0', 'b1', 'vdop', 'kl']:
+            field = outputs[k].reshape(*self.cube_shape[1:3]).cpu().numpy()
+            parameters[k] = field
+
         self.plot_parameter_overview(parameters)
+        self.plot_B_cartesian(parameters)
 
         stokes_true = outputs['stokes_true'].cpu().numpy().reshape(*self.cube_shape[1:3], 4, -1)
         stokes_pred = outputs['stokes_pred'].cpu().numpy().reshape(*self.cube_shape[1:3], 4, -1)
 
+        self.plot_stokes(stokes_pred, stokes_true)
+
+        self.plot_profile(stokes_pred, stokes_true)
+
+        # plot PSF
+        if not isinstance(self.psf, NoPSF):
+            self.plot_psf()
+
+    def plot_profile(self, stokes_pred, stokes_true):
+        y_range = stokes_true.shape[0]
+        x_range = stokes_true.shape[1]
+        pos = np.stack(np.meshgrid(np.linspace(x_range * 0.1, x_range * 0.9, 3, dtype=int),
+                                   np.linspace(y_range * 0.1, y_range * 0.9, 3, dtype=int)), -1)
+        pos = pos.reshape(-1, 2)
+        for x, y in pos:
+            fig, axs = plt.subplots(4, 1, figsize=(8, 8))
+            for i, label in enumerate(['I', 'Q', 'U', 'V']):
+                axs[i].plot(stokes_true[y, x, i], label=f'true - {label}')
+                axs[i].plot(stokes_pred[y, x, i], label=f'pred - {label}')
+                if i == 0:
+                    continue
+                # v_min_max = np.abs(stokes_true[20, 20, i]).max()
+                # v_min_max = max(1e-4, v_min_max)
+                # axs[i].set_ylim([-v_min_max, v_min_max])
+
+            [ax.legend(loc='upper right') for ax in axs]
+            # log figure
+            fig.tight_layout()
+            wandb.log({f"Profile x:{x:02d} y:{y:02d}": wandb.Image(fig)})
+            plt.close('all')
+
+    def plot_psf(self):
+        psf = self.psf()
+        psf = psf.detach().cpu().numpy()
+        fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+        im = ax.imshow(psf.T, vmin=0)
+        fig.colorbar(im)
+        fig.tight_layout()
+        wandb.log({"PSF": fig})
+        plt.close('all')
+
+    def plot_stokes(self, stokes_pred, stokes_true):
         # plot comparison of integrated stokes vectors
         integerated_stokes_true = np.abs(stokes_true).sum(-1)
         integerated_stokes_pred = np.abs(stokes_pred).sum(-1)
         fig, ax = plt.subplots(2, 4, figsize=(16, 8), dpi=100)
-        for i, label in enumerate(['I', 'Q/I', 'U/I', 'V/I']):
+        for i, label in enumerate(['I', 'Q', 'U', 'V']):
             v_min = integerated_stokes_true[:, :, i].min()
             v_max = integerated_stokes_true[:, :, i].max()
             norm = ImageNormalize(vmin=v_min, vmax=v_max)
-            im = ax[0, i].imshow(integerated_stokes_true[:, :, i], norm=norm)
+            im = ax[0, i].imshow(integerated_stokes_true[:, :, i].T, norm=norm)
             ax[0, i].set_title(f"true - {label}")
             divider = make_axes_locatable(ax[0, i])
             cax = divider.append_axes("right", size="5%", pad=0.05)
             fig.colorbar(im, cax=cax)
-            im = ax[1, i].imshow(integerated_stokes_pred[:, :, i], norm=norm)
+            im = ax[1, i].imshow(integerated_stokes_pred[:, :, i].T, norm=norm)
             ax[1, i].set_title(f"pred - {label}")
             divider = make_axes_locatable(ax[1, i])
             cax = divider.append_axes("right", size="5%", pad=0.05)
@@ -203,115 +255,54 @@ class MEModule(LightningModule):
         wandb.log({"Integrated Stokes vector - Comparison": fig})
         plt.close('all')
 
-        if self.plot_profiles:
-            y_range = stokes_true.shape[0]
-            x_range = stokes_true.shape[1]
-            pos = np.stack(np.meshgrid(np.linspace(x_range * 0.1, x_range * 0.9, 3, dtype=int),
-                                       np.linspace(y_range * 0.1, y_range * 0.9, 3, dtype=int)), -1)
-            pos = pos.reshape(-1, 2)
-            for x, y in pos:
-                fig, axs = plt.subplots(4, 1, figsize=(8, 8))
-                for i, label in enumerate(['I', 'Q', 'U', 'V']):
-                    axs[i].plot(stokes_true[y, x, i], label=f'true - {label}')
-                    axs[i].plot(stokes_pred[y, x, i], label=f'pred - {label}')
-                    if i == 0:
-                        continue
-                    # v_min_max = np.abs(stokes_true[20, 20, i]).max()
-                    # v_min_max = max(1e-4, v_min_max)
-                    # axs[i].set_ylim([-v_min_max, v_min_max])
-
-                [ax.legend(loc='upper right') for ax in axs]
-                # log figure
-                fig.tight_layout()
-                wandb.log({f"Profile x:{x:02d} y:{y:02d}": wandb.Image(fig)})
-                plt.close('all')
-
-        # plot PSF
-        if not isinstance(self.psf, NoPSF):
-            psf = self.psf()
-            psf = psf.detach().cpu().numpy()
-            fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-            im = ax.imshow(psf, vmin=0)
-            fig.colorbar(im)
-            fig.tight_layout()
-            wandb.log({"PSF": fig})
-            plt.close('all')
-
-    def plot_parameters(self, outputs):
-        parameters = {}
-        for k in ['b_field', 'theta', 'chi', 'vmac', 'damping', 'b0', 'b1', 'vdop', 'kl']:
-            field = outputs[k].reshape(*self.cube_shape[1:3]).cpu().numpy()
-            parameters[k] = field
-            plot_settings = {}
-            if k == 'theta':
-                field = field % np.pi
-                plot_settings['vmin'] = 0
-                plot_settings['vmax'] = np.pi
-                plot_settings['cmap'] = 'RdBu_r'
-            if k == 'chi':
-                field = field % np.pi
-                plot_settings['vmin'] = 0
-                plot_settings['vmax'] = np.pi
-                plot_settings['cmap'] = 'twilight'
-            if k == 'b_field':
-                b_norm = np.abs(field).max()
-                plot_settings['vmin'] = -b_norm
-                plot_settings['vmax'] = b_norm
-                plot_settings['cmap'] = 'RdBu_r'
-            if k == "b0" or k == "b1" or k == "mu":
-                pass
-            if k == "vmac" or k == "damping" or k == "kl":
-                plot_settings['vmin'] = 0.
-
-            fig, ax = plt.subplots(1, 1, figsize=(8, 8), dpi=100)
-            im = ax.imshow(field, **plot_settings)
-            fig.colorbar(im)
-
-            fig.tight_layout()
-            wandb.log({k: fig})
-            plt.close('all')
-        return parameters
-
     def plot_parameter_overview(self, parameters):
+        b = np.abs(parameters['b_field'])
+        theta = parameters['theta']
+        chi = parameters['chi']
+        # reproject vectors (theta flip with negative B)
+        b, theta, chi = to_spherical(to_cartesian(b, theta, chi))
+        chi = chi % np.pi
+        theta = theta % np.pi
+
         fig, axs = plt.subplots(2, 5, figsize=(16, 4), dpi=150)
         ax = axs[0, 0]
-        im = ax.imshow(parameters['b_field'], cmap='RdBu_r')
+        im = ax.imshow(b.T, cmap='jet', vmin=0)
         ax.set_title("B")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
         ax = axs[0, 1]
-        im = ax.imshow(parameters['theta'], cmap='RdBu_r')
+        im = ax.imshow(theta.T, cmap='RdBu_r')
         ax.set_title("Theta")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
         ax = axs[0, 2]
-        im = ax.imshow(parameters['chi'], cmap='twilight')
+        im = ax.imshow(chi.T, cmap='twilight')
         ax.set_title("Chi")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
         ax = axs[0, 3]
-        im = ax.imshow(parameters['b0'])
+        im = ax.imshow(parameters['b0'].T)
         ax.set_title("B0")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
         ax = axs[0, 4]
-        im = ax.imshow(parameters['b1'])
+        im = ax.imshow(parameters['b1'].T)
         ax.set_title("B1")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
         ax = axs[1, 0]
-        im = ax.imshow(parameters['vmac'])
+        im = ax.imshow(parameters['vmac'].T)
         ax.set_title("Vmac")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
         ax = axs[1, 1]
-        im = ax.imshow(parameters['damping'])
+        im = ax.imshow(parameters['damping'].T)
         ax.set_title("Damping")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
@@ -320,13 +311,14 @@ class MEModule(LightningModule):
         ax.set_axis_off()
 
         ax = axs[1, 3]
-        im = ax.imshow(parameters['vdop'])
+        vdop_max = np.abs(parameters['vdop']).max()
+        im = ax.imshow(parameters['vdop'].T, cmap='RdBu_r', vmin=-vdop_max, vmax=vdop_max)
         ax.set_title("Vdop")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
         ax = axs[1, 4]
-        im = ax.imshow(parameters['kl'])
+        im = ax.imshow(parameters['kl'].T)
         ax.set_title("Kl")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
@@ -336,15 +328,53 @@ class MEModule(LightningModule):
         plt.close('all')
 
 
+    def plot_B_cartesian(self, parameters):
+        b = np.abs(parameters['b_field'])
+        theta = parameters['theta']
+        chi = parameters['chi']
+        # reproject vectors (theta flip with negative B)
+        b_xyz = to_cartesian(b, theta, chi)
+
+        fig, axs = plt.subplots(1, 3, figsize=(10, 3), dpi=150)
+        ax = axs[0]
+        bx_max = np.abs(b_xyz[..., 0]).max()
+        im = ax.imshow(b_xyz[..., 0].T, cmap='RdBu_r', vmin=-bx_max, vmax=bx_max)
+        ax.set_title("Bx")
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes('right', size='5%', pad=0.05)
+        plt.colorbar(im, cax=cax)
+
+
+        ax = axs[1]
+        by_max = np.abs(b_xyz[..., 1]).max()
+        im = ax.imshow(b_xyz[..., 1].T, cmap='RdBu_r', vmin=-by_max, vmax=by_max)
+        ax.set_title("By")
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes('right', size='5%', pad=0.05)
+        plt.colorbar(im, cax=cax)
+
+        ax = axs[2]
+        bz_max = np.abs(b_xyz[..., 2]).max()
+        im = ax.imshow(b_xyz[..., 2].T, cmap='RdBu_r', vmin=-bz_max, vmax=bz_max)
+        ax.set_title("Bz")
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes('right', size='5%', pad=0.05)
+        plt.colorbar(im, cax=cax)
+
+        plt.tight_layout()
+        wandb.log({"B": fig})
+        plt.close('all')
+
+
 class PSF(nn.Module):
 
-    def __init__(self, *shape):
+    def __init__(self, shape, file):
         super().__init__()
         assert len(shape) == 2 and shape[0] % 2 == 1 and shape[1] % 2 == 1, "Invalid PSF shape"
         psf = torch.ones(*shape, dtype=torch.float32) * -1
         psf[shape[0] // 2, shape[1] // 2] = 1
         #
-        psf = np.load('/glade/work/rjarolim/data/inversion/hinode_psf_0.16.fits')['PSF']
+        psf = np.load(file)['PSF']
         psf = torch.tensor(psf, dtype=torch.float32)
         self.psf = nn.Parameter(psf, requires_grad=False)
         self.activation = nn.Softplus()
@@ -355,15 +385,17 @@ class PSF(nn.Module):
 
 class LoadPSF(nn.Module):
 
-    def __init__(self, path):
+    def __init__(self, file, crop=None):
         super().__init__()
         #
-        if path.endswith('.npz'):
-            psf = np.load(path)['PSF']
-        elif path.endswith('.fits'):
-            psf = fits.getdata(path).astype(np.float32)
+        if file.endswith('.npz'):
+            psf = np.load(file)['PSF']
+        elif file.endswith('.fits'):
+            psf = fits.getdata(file).astype(np.float32)
         else:
-            raise ValueError(f"Invalid PSF file: {path}")
+            raise ValueError(f"Invalid PSF file: {file}")
+        if crop is not None:
+            psf = psf[crop[0]:crop[1], crop[2]:crop[3]]
         psf = torch.tensor(psf, dtype=torch.float32)
         self.psf = nn.Parameter(psf, requires_grad=False)
         self.psf_shape = psf.shape