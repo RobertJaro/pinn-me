update run scripts, clean encoding models, add logging, add jacobian to output, fix validation plotting, extend vdop range

pme/evaluation/loader.py
@@ -47,7 +47,7 @@ class PINNMEOutput:
 
             stokes = torch.stack([I, Q, U, V], dim=-1)
             jac = jacobian(stokes, pred)
-
+            pred['jacobian'] = jac
 
             for key, value in pred.items():
                 if key not in parameters:
@@ -55,7 +55,7 @@ class PINNMEOutput:
                 value = value.cpu().numpy()
                 parameters[key].append(value)
 
-        parameters = {key: np.concatenate(value).reshape(*coords_shape[:-1], -1)
+        parameters = {key: np.concatenate(value).reshape(*coords_shape[:-1], *value[0].shape[1:])
                       for key, value in parameters.items()}
 
         # reproject magnetic field vector
@@ -98,8 +98,8 @@ class PINNMEOutput:
 
 
 def to_cartesian(b, inc, azi):
-    b_x = b * np.sin(inc) * np.sin(azi)
-    b_y = b * np.sin(inc) * np.cos(azi)
+    b_x = b * np.sin(inc) * np.cos(azi)
+    b_y = b * np.sin(inc) * np.sin(azi)
     b_z = b * np.cos(inc)
     return np.stack([b_x, b_y, b_z], axis=-1)
 
@@ -109,5 +109,5 @@ def to_spherical(b):
     #
     b_field = np.linalg.norm(b, axis=-1)
     inc = np.arccos(bz / (b_field + 1e-10))
-    azi = np.arctan2(bx, by)
+    azi = np.arctan2(by, bx)
     return b_field, inc, azi

pme/model.py
@@ -39,7 +39,7 @@ class PeriodicBoundary(nn.Module):
 
 class MEModel(nn.Module):
 
-    def __init__(self, in_coords, dim, encoding='positional', num_layers=8):
+    def __init__(self, in_coords, dim=256, encoding='positional', num_layers=8):
         super().__init__()
         # encoding layer
         if encoding == "periodic":
@@ -84,7 +84,7 @@ class MEModel(nn.Module):
             x = a(l(x))
         params = self.d_out(x)
         #
-        b_field = params[..., 0:1] * 1000
+        b_field = params[..., 0:1] * 1e3
         theta = params[..., 1:2] * torch.pi
         chi = params[..., 2:3] * torch.pi
         vmac = torch.sigmoid(params[..., 3:4]) * 20e3
@@ -108,85 +108,6 @@ class MEModel(nn.Module):
 
         return output
 
-
-class ParameterModel(nn.Module):
-
-    def __init__(self, in_coords, dim, num_layers=8, encoding='positional'):
-        super().__init__()
-        # encoding layer
-        if encoding == "periodic":
-            posenc = PeriodicBoundary()
-            d_in = nn.Linear(in_coords + 2, dim)
-            self.d_in = nn.Sequential(posenc, d_in)
-        elif encoding == "positional":
-            posenc = PositionalEncoding(num_freqs=20, d_input=in_coords)
-            d_in = nn.Linear(posenc.d_output, dim)
-            self.d_in = nn.Sequential(posenc, d_in)
-        elif encoding == "learned_positional":
-            posenc = LearnedPositionalEncoding(num_freqs=20, d_input=in_coords)
-            d_in = nn.Linear(posenc.d_output, dim)
-            self.d_in = nn.Sequential(posenc, d_in)
-        elif encoding == "gaussian_positional":
-            posenc = GaussianPositionalEncoding(num_freqs=20, d_input=in_coords)
-            d_in = nn.Linear(posenc.d_output, dim)
-            self.d_in = nn.Sequential(posenc, d_in)
-        elif encoding == "linear":
-            self.d_in = nn.Linear(in_coords, dim)
-        else:
-            raise ValueError(f"Unknown encoding: {encoding}")
-
-        # hidden layers
-        lin = [nn.Linear(dim, dim) for _ in range(num_layers)]
-        self.linear_layers = nn.ModuleList(lin)
-        # output layer
-        self.d_out = nn.Linear(dim, 1)
-        # activation functions
-        self.in_activation = Swish()
-        self.activations = nn.ModuleList([Swish() for _ in range(num_layers)])
-
-    def forward(self, x):
-        x = self.in_activation(self.d_in(x))
-        for l, a in zip(self.linear_layers, self.activations):
-            x = a(l(x))
-        return self.d_out(x)
-
-
-class ParametersModel(nn.Module):
-
-    def __init__(self, in_coords, dim, encoding='positional', num_layers=8):
-        super().__init__()
-        parameter_models = [ParameterModel(in_coords, dim, encoding=encoding, num_layers=num_layers) for _ in range(9)]
-        self.parameter_models = nn.ModuleList(parameter_models)
-
-    def forward(self, x):
-        params = torch.cat([m(x) for m in self.parameter_models], -1)
-        #
-        b_field = params[..., 0:1] * 1e3
-        theta = params[..., 1:2] * torch.pi
-        chi = params[..., 2:3] * torch.pi
-        vmac = torch.sigmoid(params[..., 3:4]) * 20e3
-        damping = torch.sigmoid(params[..., 4:5]) * 1
-        b0 = torch.sigmoid(params[..., 5:6])
-        b1 = torch.sigmoid(params[..., 6:7])
-        vdop = torch.tanh(params[..., 7:8]) * 1e4
-        kl = torch.sigmoid(params[..., 8:9]) * 100
-        #
-        output = {
-            "b_field": b_field,
-            "theta": theta,
-            "chi": chi,
-            "vmac": vmac,
-            "damping": damping,
-            "b0": b0,
-            "b1": b1,
-            "vdop": vdop,
-            "kl": kl,
-        }
-
-        return output
-
-
-# revert this
 class LearnedPositionalEncoding(nn.Module):
 
     def __init__(self, num_freqs, d_input):
@@ -218,46 +139,18 @@ class GaussianPositionalEncoding(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    r"""
-    Sine-cosine positional encoder for input points.
-    """
 
-    def __init__(self, d_input: int, num_freqs: int, scale_factor: float = 2., log_space: bool = True):
-        """
-
-        Parameters
-        ----------
-        d_input: number of input dimensions
-        num_freqs: number of frequencies used for encoding
-        scale_factor: factor to adjust box size limit of 2pi (default 2; 4pi)
-        log_space: use frequencies in powers of 2
-        """
+    def __init__(self, num_freqs, d_input, max_freq=8):
         super().__init__()
-        self.d_input = d_input
-        self.n_freqs = num_freqs
-        self.log_space = log_space
-        self.d_output = d_input * (1 + 2 * self.n_freqs)
+        frequencies = 2 ** torch.linspace(0, max_freq, num_freqs)
+        self.frequencies = nn.Parameter(frequencies[None, :, None], requires_grad=False)
+        self.d_output = d_input * (num_freqs * 2)
 
-        # Define frequencies in either linear or log scale
-        if self.log_space:
-            freq_bands = 2. ** torch.linspace(0., self.n_freqs - 1, self.n_freqs)
-        else:
-            freq_bands = torch.linspace(2. ** 0., 2. ** (self.n_freqs - 1), self.n_freqs)
-            print('freq bands', freq_bands)
-
-        self.register_buffer('freq_bands', freq_bands)
-        self.scale_factor = scale_factor
-
-    def forward(self, x) -> torch.Tensor:
-        r"""
-        Apply positional encoding to input.
-        """
-        f = self.freq_bands[None, :, None]
-        enc = [x,
-               (torch.sin(x[:, None, :] * f / self.scale_factor)).reshape(x.shape[0], -1),
-               (torch.cos(x[:, None, :] * f / self.scale_factor)).reshape(x.shape[0], -1)
-               ]
-        return torch.concat(enc, dim=-1)
+    def forward(self, x):
+        encoded = x[:, None, :] * torch.pi * self.frequencies
+        encoded = encoded.reshape(x.shape[0], -1)
+        encoded = torch.cat([torch.sin(encoded), torch.cos(encoded)], -1)
+        return encoded
 
 
 def jacobian(output, coords):

pme/train/data_loader.py
@@ -89,13 +89,23 @@ class GenericDataModule(LightningDataModule):
         coordinates[..., 2] /= self.pixel_per_ds  # y
 
         self.cube_shape = coordinates.shape[:3]
-        self.data_range = [[coordinates[..., 0].min(), coordinates[..., 0].max()],
-                           [coordinates[..., 1].min(), coordinates[..., 1].max()],
-                           [coordinates[..., 2].min(), coordinates[..., 2].max()]]
+        self.data_range = np.array(
+            [[coordinates[..., 0].min(), coordinates[..., 0].max()],
+             [coordinates[..., 1].min(), coordinates[..., 1].max()],
+             [coordinates[..., 2].min(), coordinates[..., 2].max()]])
 
         self.value_range = np.stack([stokes_vector.min((0, 1, 2, -1)),
-                                        stokes_vector.max((0, 1, 2, -1))], -1)
-        print('VALUE RANGE', self.value_range)
+                                     stokes_vector.max((0, 1, 2, -1))], -1)
+        print('VALUE RANGE')
+        print(f'Stokes-I: {self.value_range[0, 0]:.2f} - {self.value_range[0, 1]:.2f}')
+        print(f'Stokes-Q: {self.value_range[1, 0]:.2f} - {self.value_range[1, 1]:.2f}')
+        print(f'Stokes-U: {self.value_range[2, 0]:.2f} - {self.value_range[2, 1]:.2f}')
+        print(f'Stokes-V: {self.value_range[3, 0]:.2f} - {self.value_range[3, 1]:.2f}')
+
+        print('Coordinate Range')
+        print(f'Time: {self.data_range[0, 0]:.2f} - {self.data_range[0, 1]:.2f} ({coordinates.shape[0]})')
+        print(f'X: {self.data_range[1, 0]:.2f} - {self.data_range[1, 1]:.2f} ({coordinates.shape[1]})')
+        print(f'Y: {self.data_range[2, 0]:.2f} - {self.data_range[2, 1]:.2f} ({coordinates.shape[2]})')
 
         # plot coordinates
         ref_time = stokes_vector.shape[0] // 2

pme/train/me_module.py
@@ -16,15 +16,18 @@ from pme.train.me_atmosphere import MEAtmosphere
 
 class MEModule(LightningModule):
 
-    def __init__(self, cube_shape, lambda_config, value_range, pixel_per_ds, psf_config={'type': None},
-                 dim=256, lr_params={"start": 5e-4, "end": 5e-5, "iterations": 1e5}, lambda_stokes=[1, 1, 1, 1],
-                 encoding="positional", **kwargs):
+    def __init__(self, cube_shape, lambda_config, value_range, pixel_per_ds, psf_config=None,
+                 lr_params=None, lambda_stokes=None, model_config=None, **kwargs):
         super().__init__()
+        lr_params = lr_params if lr_params is not None else {"start": 5e-4, "end": 5e-5, "iterations": 1e5}
+        lambda_stokes = lambda_stokes if lambda_stokes is not None else [1, 1, 1, 1]
+        psf_config = psf_config if psf_config is not None else {"type": None}
 
         self.cube_shape = cube_shape
 
         # init model
-        self.parameter_model = MEModel(3, dim, encoding=encoding)
+        model_config = model_config if model_config is not None else {}
+        self.parameter_model = MEModel(3, **model_config)
 
         psf_type = psf_config.pop('type')
         if psf_type is None:
@@ -45,6 +48,12 @@ class MEModule(LightningModule):
                         indexing='ij'), -1)
         coords_psf = coords_psf[0, :, :]  # remove time axis
         coords_psf /= pixel_per_ds
+
+        print(f'PSF: {psf_shape}')
+        print(f't: {coords_psf[..., 0].min()} - {coords_psf[..., 0].max()}')
+        print(f'X: {coords_psf[..., 1].min()} - {coords_psf[..., 1].max()}')
+        print(f'Y: {coords_psf[..., 2].min()} - {coords_psf[..., 2].max()}')
+
         coords_psf = torch.tensor(coords_psf, dtype=torch.float32).reshape((1, *psf_shape, 3))
         self.coords_psf = nn.Parameter(coords_psf, requires_grad=False)
 
@@ -266,7 +275,7 @@ class MEModule(LightningModule):
 
         fig, axs = plt.subplots(2, 5, figsize=(16, 4), dpi=150)
         ax = axs[0, 0]
-        im = ax.imshow(b.T, cmap='jet', vmin=0)
+        im = ax.imshow(b.T, cmap='viridis', vmin=0)
         ax.set_title("B")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
@@ -328,7 +337,7 @@ class MEModule(LightningModule):
         plt.close('all')
 
     def plot_B_cartesian(self, parameters):
-        b = np.abs(parameters['b_field'])
+        b = parameters['b_field']
         theta = parameters['theta']
         chi = parameters['chi']
         # reproject vectors (theta flip with negative B)