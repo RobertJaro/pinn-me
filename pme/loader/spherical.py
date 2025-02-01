fix trig functions for gradients, add evaluation

pme/loader/spherical.py
@@ -110,10 +110,11 @@ class SphericalDataModule(LightningDataModule):
         shuffle_async(datasets, self.num_workers)
         # data loader with iterations based on the largest dataset
         max_ds_len = max([len(ds) for ds in datasets.values()])
+        n_ds_workers = max(self.num_workers // len(datasets), 2)
         loaders = {}
         for i, (name, dataset) in enumerate(datasets.items()):
             sampler = RandomSampler(dataset, replacement=True, num_samples=int(max_ds_len))
-            loaders[name] = DataLoader(dataset, batch_size=None, num_workers=self.num_workers,
+            loaders[name] = DataLoader(dataset, batch_size=None, num_workers=n_ds_workers,
                                        pin_memory=True, sampler=sampler)
         return loaders
 
@@ -220,7 +221,7 @@ class HMISphericalDataset(TensorsDataset):
         stokes = np.stack([I_profile, Q_profile, U_profile, V_profile], -2)
 
         # apply mask filter - coordinates + stokes for normalization
-        radius_mask = radial_distance > 0.98
+        radius_mask = radial_distance > 0.99
         cartesian_coords[radius_mask] = np.nan
         stokes[radius_mask] = np.nan
 

pme/train/me_spherical_module.py
@@ -11,6 +11,7 @@ from torch.optim.lr_scheduler import ExponentialLR
 from pme.evaluation.loader import to_spherical, to_cartesian
 from pme.model import NormalizationModule, MESphericalModel
 from pme.train.me_atmosphere import MEAtmosphere
+from pme.train.util import acos_safe, atan2_safe
 
 
 class MESphericalModule(LightningModule):
@@ -121,8 +122,9 @@ class MESphericalModule(LightningModule):
         b_rtp = torch.einsum("...ij,...j->...i", cartesian_to_spherical_transform, b_xyz)
         b_img = torch.einsum("...ij,...j->...i", rtp_to_img_transform, b_rtp)
         b_field = torch.norm(b_img, dim=-1, keepdim=True)
-        theta = torch.acos(b_img[..., 2:3] / (b_field + 1e-8))
-        chi = torch.atan2(-b_img[..., 0:1], b_img[..., 1:2])
+
+        theta = acos_safe(b_img[..., 2:3] / (b_field + 1e-8))
+        chi = atan2_safe(-b_img[..., 0:1], b_img[..., 1:2])
         # transform V
         v_xyz = torch.cat([output['v_x'], output['v_y'], output['v_z']], dim=-1)
         v_rtp = torch.einsum("...ij,...j->...i", cartesian_to_spherical_transform, v_xyz)