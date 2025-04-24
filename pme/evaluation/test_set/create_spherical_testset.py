724b395bc389d14693914d36de527fa9e3120607
momomolnar <momchil.molnar@gmail.com>
2025-04-24 16:49:59 +0300

    Fix carrington coordinates

--- pme/evaluation/test_set/create_spherical_testset.py
+++ pme/evaluation/test_set/create_spherical_testset.py
@@ -97,7 +97,7 @@ class Synthesizer():
         lambda_range = (n_lambda - 1) * lambda_step
         self.lambda_grid = np.linspace(-0.5 * lambda_range, 0.5 * lambda_range, n_lambda)
 
-    def synthesize(self, am):
+    def synthesize(self, am, xx, yy):
         if am == None:
             return {'stokes_profiles': np.nan, 'b_field': np.nan, 'theta': np.nan, 'chi': np.nan,
                     'b0': np.nan, 'b1': np.nan, 'vmac': np.nan,
@@ -106,11 +106,15 @@ class Synthesizer():
 
         atmos = MEAtmosphere(self.lambda0, self.jUp, self.jLow, self.gUp, self.gLow, self.lambda_grid)
 
-        B, theta, chi = vector_cartesian_to_polar(np.array([am['Bx'], am['By'], am['Bz']]))
+        B, theta, chi = vector_cartesian_to_polar(np.array([am['Bx'][xx, yy], am['By'][xx, yy], am['Bz'][xx, yy]]))
         I, Q, U, V = atmos.forward(torch.tensor(B), torch.tensor(theta), torch.tensor(chi),
-                                   torch.tensor(am['vmac']), torch.tensor(am['damping']),
-                                   torch.tensor(am['b0']), torch.tensor(am['b1']), torch.tensor(am['mu']),
-                                   torch.tensor(am['vdop']), torch.tensor(am['kl']))
+                                   torch.tensor(am['vmac'][xx, yy]),
+                                   torch.tensor(am['damping'][xx, yy]),
+                                   torch.tensor(am['b0'][xx, yy]),
+                                   torch.tensor(am['b1'][xx, yy]),
+                                   torch.tensor(am['mu'][xx, yy]),
+                                   torch.tensor(am['vdop'][xx, yy]),
+                                   torch.tensor(am['kl'][xx, yy]))
 
         stokes_profiles = torch.stack([I, Q, U, V], -2).cpu().numpy()
         # (x, y, n_lambda, n_stokes)
@@ -121,13 +125,15 @@ class Synthesizer():
                                                       'damping': am['damping'], 'mu': am['mu'],
                                                       'vdop': am['vdop'], 'kl': am['kl']}
 
-def observed_params(x_POS, y_POS, carrington_lon, carrington_lat, carrington_pAng,
+def observed_params(sunpy_dummy_map,
+                    carrington_lon_obs,
+                    carrington_lat_obs, carrington_pAng,
                     parameters_interpolators, sun_radius=954*u.arcsec):
     """ Function to compute the observed parameters in the local LOS frame
         Input:
-            -- x_POS, y_POS: float, astropy.units.arcsec
-                x,y coordinates in the helioprojective reference frame
-            -- carrington_lon, carrington_lat: float, astropy.units.deg
+            -- sunpy_dummy_map: sunpy.map object
+                get the x,y coordinates in the helioprojective reference frame
+            -- carrington_lon_obs, carrington_lat_obs: float, astropy.units.deg
                 carrington location of the observer
             -- parameters: dict
                 Dictionary with all the ME parameters for the synthesis
@@ -137,39 +143,48 @@ def observed_params(x_POS, y_POS, carrington_lon, carrington_lat, carrington_pAn
                 Reprojected ME parameters for the synthesis
     """
 
+    spherical_coords = all_coordinates_from_map(sunpy_dummy_map)
+    carrington_coords = spherical_coords.transform_to(frames.HeliographicCarrington(observer=sc))
+    lat, lon = carrington_coords.lat.to_value(u.rad), carrington_coords.lon.to_value(u.rad)
+    pAng = -np.deg2rad(sunpy_dummy_map.meta['CROTA2'])
 
+    sun_ang_radius = sun.angular_radius(sc.obstime)
 
-    lat_observed = np.arcsin(y_POS / sun_radius) - carrington_lat
-    lon_observed = np.arcsin(x_POS / sun_radius) - carrington_lon
+    transform_params = {}
 
-    a_matrix = image_to_spherical_matrix(lon_observed, lat_observed,
-                                         carrington_lon, carrington_lat,
-                                         pAng=carrington_pAng)
-
-    a_matrix_inverse = np.linalg.inv(a_matrix)
-
-    local_params = {}
-    transformed_params = {}
-    b_field_keys_polar_local = ['b_field', 'theta', 'chi'] # b field in the local polar frame (aka up is radial direction)
-    non_transform_keys = ['b0', 'b1', 'vmac', 'damping', 'mu', 'vdop', 'kl']
     for el in parameters_interpolators.keys():
-        local_params[el] = parameters_interpolators[el](lon_observed,
-                                                        lat_observed)
-    local_params['mu'] = compute_mu(x_POS, y_POS, sun_radius)
+        interp_params = np.array([parameters_interpolators[el](el1[0], el1[1])
+                                  for el1 in zip(lon.flatten(), lat.flatten())])
+        interp_params = interp_params.reshape(nx_image, ny_image)
+        transform_params[el] = interp_params
 
-    for el in non_transform_keys:
-        transformed_params[el] = local_params[el]
+    transform_params['mu'] = compute_mu(spherical_coords.Tx,
+                                    spherical_coords.Ty,
+                                    sun_ang_radius)
 
-    Br = local_params['b_field'] * np.cos(local_params['theta'])
-    Bt = local_params['b_field'] * np.sin(local_params['theta']) * np.sin(local_params['chi'])
-    Bp = local_params['b_field'] * np.sin(local_params['theta']) * np.cos(local_params['chi'])
+    a_matrix1 = np.array([image_to_spherical_matrix(el[0], el[1],
+                                                    sc.lat.to_value(u.rad),
+                                                    sc.lon.to_value(u.rad), pAng=pAng)
+                         for el in zip(lon.flatten(), lat.flatten())])
+    a_matrix_inverse_lin = np.array([np.linalg.inv(el) for el in a_matrix1])
 
-    B_transformed = np.matmul(a_matrix_inverse, np.array([Br, Bt, Bp]).squeeze().T)
-    transformed_params['Bx'] = B_transformed[0]
-    transformed_params['By'] = B_transformed[1]
-    transformed_params['Bz'] = B_transformed[2]
+    a_matrix = a_matrix1.reshape(nx_image, ny_image, 3, 3)
+    a_matrix_inverse = a_matrix_inverse_lin.reshape(nx_image, ny_image, 3, 3)
 
-    return transformed_params
+    Br = transform_params['b_field'] * np.cos(transform_params['theta'])
+    Bt = transform_params['b_field'] * np.sin(transform_params['theta']) * np.sin(transform_params['chi'])
+    Bp = transform_params['b_field'] * np.sin(transform_params['theta']) * np.cos(transform_params['chi'])
+
+    B_transformed = np.array([np.matmul(el[0], el[1]).squeeze().T
+                              for el in zip(a_matrix_inverse.reshape(nx_image*ny_image, 3, 3),
+                                            np.array([Br.flatten(), Bt.flatten(), Bp.flatten()]).T)])
+
+    B_transformed = B_transformed.reshape(nx_image, ny_image, 3)
+
+    transform_params['Bx'] = B_transformed[..., 0]
+    transform_params['By'] = B_transformed[..., 1]
+    transform_params['Bz'] = B_transformed[..., 2]
+    return transform_params
 
 class Carrington_map():
 
@@ -340,70 +355,71 @@ def write_testcase_file(output_dir, nx_image, ny_image, image_size,
         # define the spherical coordinate system for the data
         lat_array = np.linspace(-np.pi/2,
                                 np.pi/2, num=nx_testset) * u.rad
-        lon_array = np.linspace(-np.pi,
-                                np.pi, num=ny_testset) * u.rad
+        lon_array = np.linspace(0, 2*np.pi, num=ny_testset) * u.rad
         params_dict[el] = parameter_array
         parameters_interpolators[el] = RectBivariateSpline(lon_array, lat_array, parameter_array)
 
     ## Compute the coordinates of the sampled points in the space of the dataset
-    params_transformed = {}
+    params_transformed = np.zeros((10, nx_image, ny_image))
 
-    for xx in tqdm(range(mask_image.shape[0])):
-        for yy in range(mask_image.shape[1]):
-            if mask_image[xx, yy] == False:
-                params_transformed[f'{xx}_{yy}'] = None
-                continue
+    header = {
+        'CTYPE1': 'HPLN-TAN',
+        'CTYPE2': 'HPLT-TAN',
+        'CUNIT1': 'arcsec',
+        'CUNIT2': 'arcsec',
+        'HGLN_OBS': sc.lon.to(u.deg).value,
+        'HGLT_OBS': sc.lat.to(u.deg).value,
+        'DSUN_OBS': sc.radius.to(u.m).value,
+        'CDELT1': plate_scale[0].value,  # pixel size along x axis
+        'CDELT2': plate_scale[1].value,  # pixel size along y axis
+        'CRPIX1': nx_image // 2,  # reference pixel along x axis
+        'CRPIX2': ny_image // 2,  # reference pixel along y axis
+        'CRVAL1': 0.0,  # solar disk center longitude in arcsec
+        'CRVAL2': 0.0,  # solar disk center latitude in arcsec
+        'CROTA2': 0.0,
+        'DATE-OBS': time_string,  # date of observation
 
-            params_transformed[f'{xx}_{yy}'] = observed_params(xy_meshgrid_image_plane[0][xx, yy],
-                                                               xy_meshgrid_image_plane[1][xx, yy],
-                                                               observer_lon, observer_lat, observer_pAng,
-                                                               parameters_interpolators, sun_radius=sun_radius)
+    }
+
+    data_dummy  = np.random.rand(nx_image, ny_image)
+    sunpy_dummy_map = sunpy.map.Map(data_dummy, header)
 
 
-    Bx = make_map_from_dicts(params_transformed, 'Bx', (nx_image, ny_image))
-    By = make_map_from_dicts(params_transformed, 'By', (nx_image, ny_image))
-    Bz = make_map_from_dicts(params_transformed, 'Bz', (nx_image, ny_image))
-    b0 = make_map_from_dicts(params_transformed, 'b0', (nx_image, ny_image))
-    mu = make_map_from_dicts(params_transformed, 'mu', (nx_image, ny_image))
-
+    params_transformed = observed_params(sunpy_dummy_map,
+                                         observer_lon, observer_lat, observer_pAng,
+                                         parameters_interpolators, sun_radius=sun_radius)
 
     plot_results = True
+
     if plot_results:
-        Btot = np.sqrt(Bx**2 + By**2 + Bz**2)
-        plt.clf()
-        im1 = plt.imshow(Bz.T, cmap="seismic")
-        plt.colorbar(im1)
-        plt.savefig(os.path.join(output_dir, f"Bz_map_{observer_lon:0.3f}_d_lon_{observer_lat:0.3f}_deg_lat.png"))
-        plt.clf()
-        im1 = plt.imshow(b0.T, cmap="plasma")
-        plt.colorbar(im1)
-        plt.savefig(os.path.join(output_dir, f"b0_{observer_lon:0.3f}_d_lon_{observer_lat:0.3f}_deg_lat.png"))
-        plt.clf()
-        im1 = plt.imshow(b0.T, cmap="plasma")
-        plt.colorbar(im1)
-        plt.savefig(os.path.join(output_dir, "b0_{observer_lon:0.3f}_d_lon_{observer_lat:0.3f}_deg_lat.png"))
-        plt.clf()
-        im1 = plt.imshow(Bx.T, cmap="seismic")
-        plt.colorbar(im1)
-        plt.savefig(os.path.join(output_dir, "Bx_map_{observer_lon:0.3f}_d_lon_{observer_lat:0.3f}_deg_lat.png"))
-        plt.clf()
-        im1 = plt.imshow(By.T, cmap="seismic")
-        plt.colorbar(im1)
-        plt.savefig(os.path.join(output_dir, "By_map_{observer_lon:0.3f}_d_lon_{observer_lat:0.3f}_deg_lat.png"))
-        plt.clf()
-        im1 = plt.imshow(Btot.T, cmap="seismic")
-        plt.colorbar(im1)
-        plt.savefig(os.path.join(output_dir, f"B_map_{observer_lon:0.3f}_d_lon_{observer_lat:0.3f}_deg_lat.png"))
+        def plot_fn_quick(quantity, cmap="plasma", label="bx"):
+            plt.clf()
+            im1 = plt.imshow(quantity.T, cmap=cmap)
+            plt.colorbar(im1)
+            plt.savefig(os.path.join(output_dir,
+                                     f"{label}_map_{observer_lon:0.3f}_d_lon_{observer_lat:0.3f}_deg_lat.png"))
+
+        Btot = np.sqrt(params_transformed['Bx']**2
+                       + params_transformed['By']**2
+                       + params_transformed['Bz']**2)
+
+        plot_fn_quick(Btot, label='Btot')
+        plot_fn_quick(params_transformed['Bx'], label='Bx')
+        plot_fn_quick(params_transformed['By'], label='By')
+        plot_fn_quick(params_transformed['Bz'], label='Bz')
+        plot_fn_quick(params_transformed['mu'], label='mu')
 
     synthesizer = Synthesizer()
     spectra = {}
+
     for xx in tqdm(range(mask_image.shape[0])):
         for yy in range(mask_image.shape[1]):
-            if mask_image[xx, yy] == False or np.isnan(params_transformed[f'{xx}_{yy}']['b1']):
-                spectra[f'{xx}_{yy}'] = synthesizer.synthesize(None)
+
+            if mask_image[xx, yy] == False or np.isnan(params_transformed['b1'][xx, yy]):
+                spectra[f'{xx}_{yy}'] = synthesizer.synthesize(None, xx, yy)
                 continue
 
-            spectra[f'{xx}_{yy}'] = synthesizer.synthesize(params_transformed[f'{xx}_{yy}'])
+            spectra[f'{xx}_{yy}'] = synthesizer.synthesize(params_transformed, xx, yy)
 
     ## Create the fits files with headers compatible with the observations
     central_image_pixel_x = nx_image // 2
@@ -423,7 +439,6 @@ def write_testcase_file(output_dir, nx_image, ny_image, image_size,
                                                      (nx_image, ny_image))
 
     header = fits.Header()
-    filename = "blah.fits"
     filename = os.path.join(output_dir, filename)
     # Create PrimaryHDU for the primary array (array1)
     hdu1 = fits.PrimaryHDU(data=atmos_params)
@@ -473,17 +488,21 @@ if __name__ == '__main__':
     ny_image = 100
 
     image_size = [2000 * u.arcsec, 2000 * u.arcsec]
+    plate_scale = [image_size[0] / nx_image,
+                   image_size[1] / ny_image]
     time_string = "2010-01-01T00:00:00"
 
     # Define observer time and xyz
 
-    observer_lon = 20 * u.deg
+    observer_lon = 200 * u.deg
     observer_lat = 10 * u.deg
     observer_distance = 1 * u.AU
     obs_time = Time(time_string)
     observer_pAng = 0 * u.deg # Corresponding to CROTA angle for the observer
     sc = SkyCoord(observer_lon, observer_lat, observer_distance,
-                  obstime=obs_time, observer="self", frame="heliographic_carrington")
+                  obstime=obs_time, observer="self", frame="heliographic_stonyhurst")
+
+    filename = os.path.join(output_dir, f"spherical_syn_{observer_lon}_{observer_lat}.fits")
 
     write_testcase_file(output_dir, nx_image, ny_image, image_size,
-                        sc, filename='blah.fits')
+                        sc, filename=filename)
