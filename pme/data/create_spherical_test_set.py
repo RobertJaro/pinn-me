fix differential rotation direction, update dataset batch splitting, change test set creation

pme/data/create_spherical_test_set.py
@@ -56,7 +56,7 @@ class SphericalTestSetGenerator(TestSetGenerator):
         # add differential rotation
         latitudes = np.linspace(-np.pi / 2, np.pi / 2, v_r.shape[0]) * u.rad
         v_diff = solar_differential_rotation_velocity(latitudes).to_value(u.m / u.s)
-        v_phi -= v_diff[:, None]
+        v_phi += v_diff[:, None]
         # add to parameter dict
         input_parameters['v_r'] = v_r
         input_parameters['v_theta'] = v_theta

pme/loader/spherical.py
@@ -27,7 +27,8 @@ class SphericalDataModule(LightningDataModule):
     def __init__(self, train_config, valid_config, work_directory, seconds_per_dt=36000, Rs_per_ds=1,
                  stokes_normalization=83696.0,
                  ref_time=datetime(2010, 5, 1, 18, 58),
-                 batch_size=4096, num_workers=None):
+                 batch_size=65536, dataset_batch_size=4096,
+                 num_workers=None):
         super().__init__()
 
         ref_time = parse(ref_time) if isinstance(ref_time, str) else ref_time
@@ -36,8 +37,10 @@ class SphericalDataModule(LightningDataModule):
         # train parameters
         n_gpus = torch.cuda.device_count() if torch.cuda.is_available() else 1
         self.batch_size = batch_size * n_gpus
-        self.dataset_batch_size = batch_size // len(train_files) * n_gpus
+        self.dataset_batch_size = dataset_batch_size * n_gpus
         self.num_workers = num_workers if num_workers is not None else os.cpu_count()
+        print('Using {} GPUs'.format(n_gpus))
+        print('Using {} CPUs'.format(self.num_workers))
 
         with Pool(num_workers) as p:
             args = zip(train_files, repeat(seconds_per_dt), repeat(Rs_per_ds), repeat(ref_time),

pme/train/me_spherical_module.py
@@ -155,7 +155,7 @@ class MESphericalModule(LightningModule):
         v_xyz = torch.cat([output['v_x'], output['v_y'], output['v_z']], dim=-1)
         v_rtp = torch.einsum("...ij,...j->...i", cartesian_to_spherical_transform, v_xyz)
         v_rtp[..., 1] *= -1  # TODO do we need this?
-        v_rtp[..., 2] -= v_rot
+        v_rtp[..., 2] += v_rot
         v_img = torch.einsum("...ij,...j->...i", rtp_to_img_transform, v_rtp)
 
         v_dop = v_img[..., 2:3]