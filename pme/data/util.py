fix sign convention, switch to spherical coordinate system, add time encoding

pme/data/util.py
@@ -9,9 +9,9 @@ def spherical_to_cartesian_matrix(c):
     sin = np.sin
     cos = np.cos
     #
-    matrix = [cos(t) * cos(p), - sin(t) * cos(p), - sin(p),
-              cos(t) * sin(p), - sin(t) * sin(p), cos(p),
-              sin(t), cos(t), np.zeros_like(t)]
+    matrix = [sin(t) * cos(p), cos(t) * cos(p), - sin(p),
+              sin(t) * sin(p), cos(t) * sin(p), cos(p),
+              cos(t), -sin(t), np.zeros_like(t)]
     matrix = np.stack(matrix, axis=-1).reshape((*c.shape[:-1], 3, 3))
     #
     return matrix
@@ -22,8 +22,8 @@ def cartesian_to_spherical_matrix(c):
     sin = np.sin
     cos = np.cos
     #
-    matrix = [cos(t) * cos(p), cos(t) * sin(p), sin(t),
-              -sin(t) * cos(p), -sin(t) * sin(p), cos(t),
+    matrix = [sin(t) * cos(p), sin(t) * sin(p), cos(t),
+              cos(t) * cos(p), cos(t) * sin(p), -sin(t),
               -sin(p), cos(p), np.zeros_like(p)]
     matrix = np.stack(matrix, axis=-1).reshape((*c.shape[:-1], 3, 3))
     #
@@ -34,9 +34,9 @@ def spherical_to_cartesian(v, f=np):
     sin = f.sin
     cos = f.cos
     r, t, p = v[..., 0], v[..., 1], v[..., 2]
-    x = r * cos(t) * cos(p)
-    y = r * cos(t) * sin(p)
-    z = r * sin(t)
+    x = r * sin(t) * cos(p)
+    y = r * sin(t) * sin(p)
+    z = r * cos(t)
     return f.stack([x, y, z], -1)
 
 
@@ -45,34 +45,32 @@ def cartesian_to_spherical(v, f=np):
 
     r = (x ** 2 + y ** 2 + z ** 2) ** 0.5
     nudge = (f.abs(r) < 1e-6) * 1e-6  # assure numerical stability
-    t = f.arcsin(z / (r + nudge))
+    t = f.arccos(z / (r + nudge))
     nudge = (f.abs(x) < 1e-6) * 1e-6  # assure numerical stability
     p = f.arctan2(y, x + nudge)
 
     return f.stack([r, t, p], -1)
 
-def image_to_spherical_matrix(lon, lat, lonc, latc, pAng, f=np):
-    sin = f.sin
-    cos = f.cos
+def image_to_spherical_matrix(lon, lat, lonc, latc, pAng):
+    sin = np.sin
+    cos = np.cos
 
-    a11 = -sin(latc) * sin(pAng) * sin(lon - lonc) + cos(pAng) * cos(lon - lonc)
-    a12 = sin(latc) * cos(pAng) * sin(lon - lonc) + sin(pAng) * cos(lon - lonc)
-    a13 = -cos(latc) * sin(lon - lonc)
-    a21 = -sin(lat) * (sin(latc) * sin(pAng) * cos(lon - lonc) + cos(pAng) * sin(lon - lonc)) - cos(lat) * cos(
+    k11 = cos(lat) * (sin(latc) * sin(pAng) * cos(lon - lonc) + cos(pAng) * sin(lon - lonc)) - sin(lat) * cos(
         latc) * sin(pAng)
-    a22 = sin(lat) * (sin(latc) * cos(pAng) * cos(lon - lonc) - sin(pAng) * sin(lon - lonc)) + cos(lat) * cos(
+    k12 = -cos(lat) * (sin(latc) * cos(pAng) * cos(lon - lonc) - sin(pAng) * sin(lon - lonc)) + sin(lat) * cos(
         latc) * cos(pAng)
-    a23 = -cos(latc) * sin(lat) * cos(lon - lonc) + sin(latc) * cos(lat)
-    a31 = cos(lat) * (sin(latc) * sin(pAng) * cos(lon - lonc) + cos(pAng) * sin(lon - lonc)) - sin(lat) * cos(
-        latc) * sin(pAng)
-    a32 = -cos(lat) * (sin(latc) * cos(pAng) * cos(lon - lonc) - sin(pAng) * sin(lon - lonc)) + sin(lat) * cos(
-        latc) * cos(pAng)
-    a33 = cos(lat) * cos(latc) * cos(lon - lonc) + sin(lat) * sin(latc)
+    k13 = cos(lat) * cos(latc) * cos(lon - lonc) + sin(lat) * sin(latc)
+    k21 = sin(lat) * (sin(latc) * sin(pAng) * cos(lon - lonc) + cos(pAng) * sin(lon - lonc)) + cos(lat) * cos(latc) * sin(pAng)
+    k22 = -sin(lat) * (sin(latc) * cos(pAng) * cos(lon - lonc) - sin(pAng) * sin(lon - lonc)) - cos(lat) * cos(latc) * cos(pAng)
+    k23 = sin(lat) * cos(latc) * cos(lon - lonc) - cos(lat) * sin(latc)
+    k31 = -sin(latc) * sin(pAng) * sin(lon - lonc) + cos(pAng) * cos(lon - lonc)
+    k32 = sin(latc) * cos(pAng) * sin(lon - lonc) + sin(pAng) * cos(lon - lonc)
+    k33 = -cos(latc) * sin(lon - lonc)
 
-    a_matrix = np.stack([a31, a32, a33, a21, a22, a23, a11, a12, a13], axis=-1)
+
+    a_matrix = np.stack([k11, k12, k13, k21, k22, k23, k31, k32, k33], axis=-1)
     a_matrix = a_matrix.reshape((*a_matrix.shape[:-1], 3, 3))
     return a_matrix
-
 
 def solar_differential_rotation_velocity(latitude):
     """

pme/encoding.py
@@ -64,8 +64,8 @@ class ProgressiveFourierEncoding(nn.Module):
 
         sin = torch.sin(freqs) * self.weights[..., :, None]  # [..., n_freqs, in_dim]
         cos = torch.cos(freqs) * self.weights[..., :, None]  # [..., n_freqs, in_dim]
-        sin = sin.view(*sin.shape[:-2], -1) # [..., n_freqs * in_dim]
-        cos = cos.view(*cos.shape[:-2], -1) # [..., n_freqs * in_dim]
+        sin = sin.view(*sin.shape[:-2], -1)  # [..., n_freqs * in_dim]
+        cos = cos.view(*cos.shape[:-2], -1)  # [..., n_freqs * in_dim]
         feat = torch.cat([coords, sin, cos], dim=-1)  # [..., (2 * n_freqs + 1) * in_dim]
 
         return feat  # [B, out_dim]

pme/evaluation/spherical/load_ref_map.py
@@ -10,7 +10,7 @@ from sunpy.coordinates import frames
 from sunpy.map import Map, all_coordinates_from_map
 
 from pme.data.util import spherical_to_cartesian, cartesian_to_spherical_matrix, \
-    image_to_spherical_matrix
+    image_to_spherical_matrix, spherical_to_cartesian_matrix
 from pme.evaluation.loader import PINNMEOutput
 
 if __name__ == '__main__':
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     lat, lon = coords.lat.to_value(u.rad), coords.lon.to_value(u.rad)
     r = np.ones_like(lat)  # coords.radius.to_value(u.solRad)
 
-    spherical_coords = np.stack([r, lat, lon], axis=-1)
+    spherical_coords = np.stack([r, np.pi / 2 - lat, lon], axis=-1)
     #
     cartesian_coords = spherical_to_cartesian(spherical_coords)
     time_coords = np.ones((*cartesian_coords.shape[:-1], 1), dtype=np.float32) * normalized_time
@@ -98,7 +98,7 @@ if __name__ == '__main__':
     b_img_ref = np.stack([b_xi, b_eta, b_zeta], axis=-1)
 
     b_rtp_ref = np.einsum('...ij,...j->...i', a_matrix, b_img_ref)
-    spherical_to_cartesian_matrix = np.linalg.inv(cartesian_to_spherical_transform)
+    spherical_to_cartesian_matrix = spherical_to_cartesian_matrix(spherical_coords)
     b_xyz_ref = np.einsum('...ij,...j->...i', spherical_to_cartesian_matrix, b_rtp_ref)
     ########################################################################################################################
     # Plot subframe in B_r, B_theta, B_phi
@@ -270,7 +270,7 @@ if __name__ == '__main__':
     ax.set_title('Radius')
 
     ax = axs[0, 1]
-    im = ax.imshow(np.rad2deg(spherical_coords[..., 1]), cmap='PiYG', origin='lower', vmin=-90, vmax=90)
+    im = ax.imshow(np.rad2deg(spherical_coords[..., 1]), cmap='PiYG', origin='lower', vmin=0, vmax=180)
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
     fig.colorbar(im, cax=cax, orientation='vertical', label='Latitude [deg]')

pme/evaluation/spherical/load_ref_series.py
@@ -52,9 +52,10 @@ if __name__ == '__main__':
 
         coords = all_coordinates_from_map(ref_map).transform_to(frames.HeliographicCarrington)
         lat, lon = coords.lat.to_value(u.rad), coords.lon.to_value(u.rad)
+        colat = np.pi / 2 - lat  # convert to colatitude
         r = np.ones_like(lat)  # coords.radius.to_value(u.solRad)
 
-        spherical_coords = np.stack([r, lat, lon], axis=-1)
+        spherical_coords = np.stack([r, colat, lon], axis=-1)
         #
         cartesian_coords = spherical_to_cartesian(spherical_coords) / pinnme.Rs_per_ds
         time_coords = np.ones((*cartesian_coords.shape[:-1], 1), dtype=np.float32) * normalized_time
@@ -70,12 +71,10 @@ if __name__ == '__main__':
         b_xyz = np.concatenate([parameter_cube['b_x'], parameter_cube['b_y'], parameter_cube['b_z']], axis=-1)
         b_xyz *= pinnme.gauss_per_dB
         b_rtp = np.einsum('...ij,...j->...i', cartesian_to_spherical_transform, b_xyz)
-        # b_rtp[..., 1] *= -1  # flip theta component
 
         v_xyz = np.concatenate([parameter_cube['v_x'], parameter_cube['v_y'], parameter_cube['v_z']], axis=-1)
         v_xyz *= pinnme.meters_per_ds / pinnme.seconds_per_dt / 1000  # convert to km/s
         v_rtp = np.einsum('...ij,...j->...i', cartesian_to_spherical_transform, v_xyz)
-        # v_rtp[..., 1] *= -1  # flip theta component
 
         b_img = np.einsum('...ij,...j->...i', rtp_to_img_transform, b_rtp)
         v_img = np.einsum('...ij,...j->...i', rtp_to_img_transform, v_rtp)
@@ -159,11 +158,11 @@ if __name__ == '__main__':
         ax.set_title(r'$v_\text{LOS}$ [km/s]')
 
         ax = axs[3, 1]
-        im = ax.imshow(np.rad2deg(spherical_coords[..., 1]), cmap='RdBu_r', vmin=-90, vmax=90, origin='lower')
+        im = ax.imshow(np.rad2deg(spherical_coords[..., 1]), cmap='RdBu_r', vmin=0, vmax=180, origin='lower')
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-        fig.colorbar(im, cax=cax, orientation='vertical', label=r'Latitude [deg]')
-        ax.set_title(r'Latitude [deg]')
+        fig.colorbar(im, cax=cax, orientation='vertical', label=r'Colatitude [deg]')
+        ax.set_title(r'Colatitude [deg]')
 
         ax = axs[3, 2]
         im = ax.imshow(np.rad2deg(spherical_coords[..., 2]), cmap='twilight', vmin=0, vmax=360, origin='lower')

pme/evaluation/spherical/load_series.py
@@ -36,15 +36,16 @@ if __name__ == '__main__':
 
     # resolution = 0.05 # degrees
 
-    # latitude_range = [-30, -10]
-    # longitude_range = [25, 45]
-    resolution = 1
-    latitude_range = [-90, 90]
-    longitude_range = [0, 360]
+    latitude_range = [-30, -10]
+    longitude_range = [25, 45]
+    # latitude_range = [-90, 90]
+    # longitude_range = [0, 360]
+    resolution = 0.1
 
     latitude = np.deg2rad(np.linspace(latitude_range[0], latitude_range[1], int((latitude_range[1] - latitude_range[0] + 1) // resolution)))
+    colatitude = np.pi / 2 - latitude  # convert to colatitude
     longitude = np.deg2rad(np.linspace(longitude_range[0], longitude_range[1], int((longitude_range[1] - longitude_range[0] + 1) // resolution)))
-    spherical_coords = np.stack(np.meshgrid([1], latitude, longitude, indexing='ij'), -1)  # r, theta, phi
+    spherical_coords = np.stack(np.meshgrid([1], colatitude, longitude, indexing='ij'), -1)  # r, theta, phi
     spherical_coords = spherical_coords.squeeze(0)
     cartesian_coords = spherical_to_cartesian(spherical_coords)
 

pme/loader/spherical.py
@@ -150,7 +150,7 @@ class SphericalDataModule(LightningDataModule):
         cax = divider.append_axes('right', size='5%', pad=0.05)
         fig.colorbar(im, cax=cax, orientation='vertical', label='Integrated V')
 
-        im = axs[1].imshow(ds.latitude, origin='lower', cmap='seismic', vmin=-np.pi / 2, vmax=np.pi / 2)
+        im = axs[1].imshow(ds.latitude, origin='lower', cmap='seismic', vmin=0, vmax=np.pi)
         divider = make_axes_locatable(axs[1])
         cax = divider.append_axes('right', size='5%', pad=0.05)
         fig.colorbar(im, cax=cax, orientation='vertical', label='Latitude [rad]')
@@ -403,7 +403,8 @@ def load_map_data(s_map):
     lat, lon = carrington_coords.lat.to_value(u.rad), carrington_coords.lon.to_value(u.rad)
     r = np.ones_like(lon)  # carrington_coords.radius
     # r = r * u.solRad if r.unit == u.dimensionless_unscaled else r
-    carrington_coords = np.stack([r, lat, lon], -1)
+    # convert latitude to colatitude
+    carrington_coords = np.stack([r,  np.pi / 2 - lat, lon], -1)
 
     # create rtp transform
     cartesian_to_spherical_transform = cartesian_to_spherical_matrix(carrington_coords)

pme/model.py
@@ -114,7 +114,7 @@ class GenericModel(nn.Module):
             d_in = nn.Linear(posenc.d_output, dim)
             self.d_in = nn.Sequential(posenc, d_in)
         elif encoding == "gaussian":
-            posenc = GaussianPositionalEncoding(d_input=in_dim, scale=64)#, scale=8)
+            posenc = GaussianPositionalEncoding(d_input=in_dim, scale=64)
             d_in = nn.Linear(posenc.d_output, dim)
             self.d_in = nn.Sequential(posenc, d_in)
         elif encoding == "progressive_fourier":
@@ -155,18 +155,34 @@ class GenericModel(nn.Module):
         out = self.d_out(x)
         return out
 
+
 class MESphericalModel(SirenModel):
 
-    def __init__(self, **kwargs):
+    def __init__(self, vector_potential=False, **kwargs):
         super().__init__(in_dim=4, out_dim=13, **kwargs)
+        self.vector_potential = vector_potential
 
     def forward(self, x):
         params = super().forward(x)
         #
-        b_x = params[..., 0:1]
-        b_y = params[..., 1:2]
-        b_z = params[..., 2:3]
-        #
+        if self.vector_potential:
+            a = params[..., 0:3]
+            jac_matrix = jacobian(a, x)
+            dAy_dx = jac_matrix[:, 1, 1]
+            dAz_dx = jac_matrix[:, 2, 1]
+            dAx_dy = jac_matrix[:, 0, 2]
+            dAz_dy = jac_matrix[:, 2, 2]
+            dAx_dz = jac_matrix[:, 0, 3]
+            dAy_dz = jac_matrix[:, 1, 3]
+            # B = curl(A)
+            b_x = (dAz_dy - dAy_dz)[..., None]
+            b_y = (dAx_dz - dAz_dx)[..., None]
+            b_z = (dAy_dx - dAx_dy)[..., None]
+        else:
+            b_x = params[..., 0:1]
+            b_y = params[..., 1:2]
+            b_z = params[..., 2:3]
+
         vmac = torch.sigmoid(params[..., 4:5]) * 20e3
         damping = torch.sigmoid(params[..., 5:6]) * 1
         b0 = torch.sigmoid(params[..., 6:7])
@@ -215,6 +231,7 @@ class NormalizationModule(nn.Module):
         stokes = torch.asinh(stokes * 1e1) / self.stretch
         return stokes
 
+
 class INormalizationModule(nn.Module):
 
     def __init__(self):
@@ -229,7 +246,7 @@ class INormalizationModule(nn.Module):
 
 
 class ProjectionModel(SirenModel):
-    def __init__(self, Mm_per_ds, max_shift_Mm = 1, **kwargs):
+    def __init__(self, Mm_per_ds, max_shift_Mm=1, **kwargs):
         super().__init__(4, 1, dim=32, n_layers=4, **kwargs)
         self.max_shift = max_shift_Mm / Mm_per_ds  # convert to model units
 

pme/train/me_spherical_module.py
@@ -135,8 +135,8 @@ class MESphericalModule(LightningModule):
             ds_stokes_pred = torch.stack([I, Q, U, V], dim=-2)
             ds_stokes_true = batch[ds_id]['stokes']
 
-            stokes_true_normalized.append(self.normalization(ds_stokes_true / ds_mu[..., None]))
-            stokes_pred_normalized.append(self.normalization(ds_stokes_pred / ds_mu[..., None]))
+            stokes_true_normalized.append(self.normalization(ds_stokes_true))
+            stokes_pred_normalized.append(self.normalization(ds_stokes_pred))
 
         #################################################
         # compute stokes loss
@@ -272,9 +272,9 @@ class MESphericalModule(LightningModule):
         # y = r * cos(t) * sin(p)
         # z = r * sin(t)
         spherical_coords = cartesian_to_spherical(coords[..., 1:], torch)
-        dx_dr = torch.cos(spherical_coords[..., 1]) * torch.cos(spherical_coords[..., 2])
-        dy_dr = torch.cos(spherical_coords[..., 1]) * torch.sin(spherical_coords[..., 2])
-        dz_dr = torch.sin(spherical_coords[..., 1])
+        dx_dr = torch.sin(spherical_coords[..., 1]) * torch.cos(spherical_coords[..., 2])
+        dy_dr = torch.sin(spherical_coords[..., 1]) * torch.sin(spherical_coords[..., 2])
+        dz_dr = torch.cos(spherical_coords[..., 1])
         dBx_dr = dBx_dx * dx_dr + dBx_dy * dy_dr + dBx_dz * dz_dr
         dBy_dr = dBy_dx * dx_dr + dBy_dy * dy_dr + dBy_dz * dz_dr
         dBz_dr = dBz_dx * dx_dr + dBz_dy * dy_dr + dBz_dz * dz_dr
@@ -334,7 +334,8 @@ class MESphericalModule(LightningModule):
 
         # transform to carrington frame --> add rotation velocity
         spherical_coords = cartesian_to_spherical(coords[..., 1:], torch)
-        latitude = spherical_coords[..., 1]  # theta in spherical coordinates
+        colatitude = spherical_coords[..., 1]  # theta in spherical coordinates
+        latitude = torch.pi / 2 - colatitude  # convert to latitude
         v_rot = carrington_rotation_velocity(latitude)  # in m/s
         v_rot = v_rot / self.meters_per_ds * self.seconds_per_dt  # convert to ds/dt (model units)
 
@@ -401,8 +402,8 @@ class MESphericalModule(LightningModule):
         I, Q, U, V = self.forward_models[instrument_id](**forward_params, mu=mu, lambda_grid=lambda_grid)
         stokes_pred = torch.stack([I, Q, U, V], dim=-2)
 
-        stokes_true_normalized = self.normalization(stokes_true / mu[..., None])
-        stokes_pred_normalized = self.normalization(stokes_pred / mu[..., None])
+        stokes_true_normalized = self.normalization(stokes_true)
+        stokes_pred_normalized = self.normalization(stokes_pred)
 
         diff = torch.abs(stokes_true_normalized - stokes_pred_normalized)
 