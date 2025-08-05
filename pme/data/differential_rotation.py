fix sign convention of doppler velocity, add evaluation,refactor: update training configurations and improve data loading functions

pme/data/differential_rotation.py
@@ -62,7 +62,7 @@ def solar_differential_rotation_velocity_torch(latitude_rad: torch.Tensor) -> to
     v = R_sun * omega_rad_per_sec * torch.cos(latitude_rad)
     return v  # in m/s
 
-def carrington_rotation_velocity(latitude):
+def carrington_rotation_velocity(latitude, f=torch):
     R_sun = 6.957e8  # Solar radius in meters
-    rotation_rate = 2 * torch.pi / (27.2753 * 24 * 3600) # rad/s
-    return R_sun * rotation_rate * torch.cos(latitude)  # in m/s
+    rotation_rate = 2 * f.pi / (27.2753 * 24 * 3600) # rad/s
+    return R_sun * rotation_rate * f.cos(latitude)  # in m/s

pme/evaluation/spherical/load_ref_map.py
@@ -3,15 +3,18 @@ import os.path
 
 import numpy as np
 from astropy import units as u
+from astropy.coordinates import SkyCoord
 from matplotlib import pyplot as plt
 from matplotlib.colors import Normalize, SymLogNorm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from sunpy.coordinates import frames
 from sunpy.map import Map, all_coordinates_from_map
 
+from pme.data.differential_rotation import carrington_rotation_velocity
 from pme.data.util import spherical_to_cartesian, cartesian_to_spherical_matrix, \
     image_to_spherical_matrix, spherical_to_cartesian_matrix
 from pme.evaluation.loader import PINNMEOutput
+from pme.loader.spherical import load_v_observer_LOS
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a video from a PINN ME file')
@@ -20,14 +23,10 @@ if __name__ == '__main__':
     parser.add_argument('--ref_map_inc', type=str, help='the path to the reference map inc')
     parser.add_argument('--ref_map_azi', type=str, help='the path to the reference map azi')
     parser.add_argument('--ref_map_disambig', type=str, help='the path to the reference map disambig')
+    parser.add_argument('--ref_map_vlos_mag', type=str, help='the path to the reference map vlos_mag')
     parser.add_argument('--output', type=str, help='the path to the output file', default=None)
+    parser.add_argument('--hpc_range', type=float, nargs=4, default=None, required=False)
     args = parser.parse_args()
-
-    # y_min = 2048 - 1024
-    # y_max = y_min + 2048
-    # x_min = 2048 - 1024
-    # x_max = x_min + 2048
-    x_min = x_max = y_min = y_max = None
 
     in_path = args.input
 
@@ -40,6 +39,13 @@ if __name__ == '__main__':
 
     # load reference maps
     ref_map = Map(args.ref_map_fld)
+
+    # convert HPC min/max to world pixel coordinates
+    if args.hpc_range is not None:
+        min_hpc_x, max_hpc_x, min_hpc_y, max_hpc_y = args.hpc_range
+        bl_coord = SkyCoord(Tx=min_hpc_x * u.arcsec, Ty=min_hpc_y * u.arcsec, frame=ref_map.coordinate_frame)
+        tr_coord = SkyCoord(Tx=max_hpc_x * u.arcsec, Ty=max_hpc_y * u.arcsec, frame=ref_map.coordinate_frame)
+        ref_map = ref_map.submap(bottom_left=bl_coord, top_right=tr_coord)
 
     # load time
     target_time = ref_map.date.to_datetime()
@@ -64,10 +70,6 @@ if __name__ == '__main__':
     parameter_cube = pinnme.load_parameters(coords=coords)
     b_xyz= np.concatenate([parameter_cube['b_x'], parameter_cube['b_y'], parameter_cube['b_z']], axis=-1) * pinnme.gauss_per_dB
     b_rtp = np.einsum('...ij,...j->...i', cartesian_to_spherical_transform, b_xyz)
-
-    v_xyz = np.concatenate([parameter_cube['v_x'], parameter_cube['v_y'], parameter_cube['v_z']], axis=-1) * pinnme.meters_per_ds / pinnme.seconds_per_dt
-    v_rtp = np.einsum('...ij,...j->...i', cartesian_to_spherical_transform, v_xyz)
-
     b_img = np.einsum('...ij,...j->...i', rtp_to_img_transform, b_rtp)
 
     fld = np.linalg.norm(b_img, axis=-1, keepdims=True)
@@ -77,12 +79,28 @@ if __name__ == '__main__':
     inc = np.rad2deg(inc)
     azi = np.rad2deg(azi)
 
+    v_xyz = np.concatenate([parameter_cube['v_x'], parameter_cube['v_y'], parameter_cube['v_z']], axis=-1) * pinnme.meters_per_ds / pinnme.seconds_per_dt
+    v_rtp = np.einsum('...ij,...j->...i', cartesian_to_spherical_transform, v_xyz)
+    v_img = np.einsum('...ij,...j->...i', rtp_to_img_transform, v_rtp)
+
     ########################################################################################################################
     # load reference map
-    fld_ref = Map(args.ref_map_fld).data
-    inc_ref = Map(args.ref_map_inc).data
-    azi_ref = Map(args.ref_map_azi).data
-    amb_ref = Map(args.ref_map_disambig).data
+    fld_ref = Map(args.ref_map_fld).reproject_to(ref_map.wcs).data
+    inc_ref = Map(args.ref_map_inc).reproject_to(ref_map.wcs).data
+    azi_ref = Map(args.ref_map_azi).reproject_to(ref_map.wcs).data
+    amb_ref = Map(args.ref_map_disambig).reproject_to(ref_map.wcs).data
+    vlos_ref = Map(args.ref_map_vlos_mag).reproject_to(ref_map.wcs).data
+    vlos_ref = vlos_ref / 100  # convert cm/s to m/s
+
+    # correct for observer LOS
+    v_obs_los = load_v_observer_LOS(ref_map)
+    vlos_ref = vlos_ref - v_obs_los
+
+    # correct for carrington rotation
+    v_carr = carrington_rotation_velocity(latitude=lat, f=np)
+    v_carr_rtp = np.stack([np.zeros_like(v_carr), np.zeros_like(v_carr), v_carr], axis=-1)
+    v_carr_img = np.einsum('...ij,...j->...i', rtp_to_img_transform, v_carr_rtp)
+    vlos_ref = vlos_ref + v_carr_img[..., 2]  # subtract LOS component of carrington rotation velocity
 
     # disambiguate
     amb_weak = 2
@@ -153,9 +171,6 @@ if __name__ == '__main__':
     [ax.set_ylabel(' ') for ax in axs.flatten()]
     [ax.set_ylabel('Latitude [deg]') for ax in axs[:, 0]]
     [ax.set_xlabel('Longitude [deg]') for ax in axs[-1]]
-
-    [ax.set_xlim(x_min, x_max) for ax in axs.flatten()]
-    [ax.set_ylim(y_min, y_max) for ax in axs.flatten()]
 
     # add subtitle with date
     plt.suptitle(f'Map at {target_time}', fontsize=16)
@@ -246,9 +261,6 @@ if __name__ == '__main__':
     fig.colorbar(im, cax=cax, orientation='vertical', label=r'$\phi$ [deg]')
 
 
-    [ax.set_xlim(x_min, x_max) for ax in axs.flatten()]
-    [ax.set_ylim(y_min, y_max) for ax in axs.flatten()]
-
     [ax.set_xlabel(' ') for ax in axs.flatten()]
     [ax.set_ylabel(' ') for ax in axs.flatten()]
     axs[0, 0].set_ylabel('PINN ME')
@@ -308,9 +320,6 @@ if __name__ == '__main__':
     [ax.set_ylabel(' ') for ax in axs.flatten()]
     axs[0, 0].set_ylabel('Spherical Coordinates')
     axs[1, 0].set_ylabel('Cartesian Coordinates')
-
-    [ax.set_xlim(x_min, x_max) for ax in axs.flatten()]
-    [ax.set_ylim(y_min, y_max) for ax in axs.flatten()]
 
     fig.tight_layout()
     plt.savefig(os.path.join(out_path, 'coordinates.jpg'), dpi=300)
@@ -388,9 +397,6 @@ if __name__ == '__main__':
     axs[0, 0].set_ylabel('Spherical Coordinates')
     axs[1, 0].set_ylabel('Cartesian Coordinates')
 
-    [ax.set_xlim(x_min, x_max) for ax in axs.flatten()]
-    [ax.set_ylim(y_min, y_max) for ax in axs.flatten()]
-
     fig.tight_layout()
     plt.savefig(os.path.join(out_path, 'cartesian.jpg'), dpi=300)
     plt.close()
@@ -399,56 +405,64 @@ if __name__ == '__main__':
     # plot velocity
     v_norm = Normalize(vmin=-2000, vmax=2000)
 
-    fig, axs = plt.subplots(2, 3, figsize=(10, 5), subplot_kw={'projection': ref_map})
+    fig, axs = plt.subplots(3, 3, figsize=(12, 8), subplot_kw={'projection': ref_map})
 
     ax = axs[0, 0]
-    im = ax.imshow(b_rtp[..., 0], cmap='gray', origin='lower', vmin=-1000, vmax=1000)
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-    fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{r}$ [G]')
-    ax.set_title('PINN ME $B_r$')
-
-    ax = axs[0, 1]
-    im = ax.imshow(b_rtp[..., 1], cmap='gray', origin='lower', vmin=-1000, vmax=1000)
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-    fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{t}$ [G]')
-    ax.set_title('PINN ME $B_t$')
-
-    ax = axs[0, 2]
-    im = ax.imshow(b_rtp[..., 2], cmap='gray', origin='lower', vmin=-1000, vmax=1000)
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-    fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{p}$ [G]')
-    ax.set_title('PINN ME $B_p$')
-
-    ax = axs[1, 0]
-    im = ax.imshow(v_rtp[..., 0], cmap='RdBu', origin='lower', norm=v_norm)
+    im = ax.imshow(v_rtp[..., 0], cmap='seismic', origin='lower', norm=v_norm)
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
     fig.colorbar(im, cax=cax, orientation='vertical', label=r'$v_\text{r}$ [m/s]')
     ax.set_title('PINN ME $v_r$')
 
-    ax = axs[1, 1]
-    im = ax.imshow(v_rtp[..., 1], cmap='RdBu', origin='lower', norm=v_norm)
+    ax = axs[0, 1]
+    im = ax.imshow(v_rtp[..., 1], cmap='seismic', origin='lower', norm=v_norm)
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
     fig.colorbar(im, cax=cax, orientation='vertical', label=r'$v_\text{t}$ [m/s]')
     ax.set_title('PINN ME $v_t$')
 
-    ax = axs[1, 2]
-    im = ax.imshow(v_rtp[..., 2], cmap='RdBu', origin='lower', norm=v_norm)
+    ax = axs[0, 2]
+    im = ax.imshow(v_rtp[..., 2], cmap='seismic', origin='lower', norm=v_norm)
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
     fig.colorbar(im, cax=cax, orientation='vertical', label=r'$v_\text{p}$ [m/s]')
     ax.set_title('PINN ME $v_p$')
 
+    ax = axs[1, 0]
+    im = ax.imshow(v_img[..., 0], cmap='seismic', origin='lower', norm=v_norm)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+    fig.colorbar(im, cax=cax, orientation='vertical', label=r'$v_x$ [m/s]')
+    ax.set_title('PINN ME $v_x$')
+
+    ax = axs[1, 1]
+    im = ax.imshow(v_img[..., 1], cmap='seismic', origin='lower', norm=v_norm)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+    fig.colorbar(im, cax=cax, orientation='vertical', label=r'$v_y$ [m/s]')
+    ax.set_title('PINN ME $v_y$')
+
+    ax = axs[1, 2]
+    im = ax.imshow(v_img[..., 2], cmap='seismic', origin='lower', norm=v_norm)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+    fig.colorbar(im, cax=cax, orientation='vertical', label=r'$v_z$ [m/s]')
+    ax.set_title('PINN ME $v_z$')
+
+    axs[2, 0].axis('off')
+    axs[2, 1].axis('off')
+
+    ax = axs[2, 2]
+    im = ax.imshow(vlos_ref, cmap='seismic', origin='lower', norm=v_norm)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+    fig.colorbar(im, cax=cax, orientation='vertical', label=r'$v_\text{los}$ [m/s]')
+    ax.set_title(r'Reference $v_\text{LOS}$')
+
     [ax.set_xlabel(' ') for ax in axs.flatten()]
     [ax.set_ylabel(' ') for ax in axs.flatten()]
     [ax.set_ylabel('Latitude [deg]') for ax in axs[:, 0]]
     [ax.set_xlabel('Longitude [deg]') for ax in axs[-1]]
-    [ax.set_xlim(x_min, x_max) for ax in axs.flatten()]
-    [ax.set_ylim(y_min, y_max) for ax in axs.flatten()]
     fig.tight_layout()
     plt.savefig(os.path.join(out_path, 'velocity.jpg'), dpi=300)
     plt.close()

pme/evaluation/spherical/load_series.py
@@ -20,6 +20,9 @@ if __name__ == '__main__':
     parser.add_argument('--input', type=str, help='the path to the input file')
     parser.add_argument('--ref_maps', type=str, help='the path to the reference map fld')
     parser.add_argument('--output', type=str, help='the path to the output file', default=None, required=False)
+    parser.add_argument('--latitude_range', type=float, nargs=2, default=[-90, 90], help='the latitude range in degrees')
+    parser.add_argument('--longitude_range', type=float, nargs=2, default=[0, 360], help='the longitude range in degrees')
+    parser.add_argument('--resolution', type=float, default=1, help='the resolution in degrees')
     args = parser.parse_args()
 
     in_path = args.input
@@ -36,11 +39,11 @@ if __name__ == '__main__':
 
     # resolution = 0.05 # degrees
 
-    latitude_range = [-30, -10]
-    longitude_range = [25, 45]
-    # latitude_range = [-90, 90]
-    # longitude_range = [0, 360]
-    resolution = 0.1
+    # latitude_range = [-30, -10]
+    # longitude_range = [25, 45]
+    latitude_range = args.latitude_range
+    longitude_range = args.longitude_range
+    resolution = args.resolution
 
     latitude = np.deg2rad(np.linspace(latitude_range[0], latitude_range[1], int((latitude_range[1] - latitude_range[0] + 1) // resolution)))
     colatitude = np.pi / 2 - latitude  # convert to colatitude
@@ -56,7 +59,8 @@ if __name__ == '__main__':
 
     for i, target_time in tqdm(enumerate(times), total=len(times)):
         file_path = os.path.join(out_path, f'step{i:03d}.jpg')
-
+        if os.path.exists(file_path):
+            continue
 
         normalized_time = pinnme._normalize_time(target_time)
         time_coords = np.ones((*cartesian_coords.shape[:-1], 1), dtype=np.float32) * normalized_time
@@ -132,55 +136,55 @@ if __name__ == '__main__':
         plt.savefig(file_path, dpi=300)
         plt.close()
 
-        fig, axs = plt.subplots(2, 3, figsize=(10, 5))
-
-        ax = axs[0, 0]
-        im = ax.imshow(b_xyz[..., 0], cmap='gray', norm=b_norm, origin='lower', extent=extent)
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-        fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{r}$ [G]')
-        ax.set_title(r'$B_\text{x}$ [G]')
-
-        ax = axs[0, 1]
-        im = ax.imshow(b_xyz[..., 1], cmap='gray', norm=b_norm, origin='lower', extent=extent)
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-        fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{t}$ [G]')
-        ax.set_title(r'$B_\text{y}$ [G]')
-
-        ax = axs[0, 2]
-        im = ax.imshow(b_xyz[..., 2], cmap='gray', norm=b_norm, origin='lower', extent=extent)
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-        fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{p}$ [G]')
-        ax.set_title(r'$B_\text{z}$ [G]')
-
-        ax = axs[1, 0]
-        im = ax.imshow(cartesian_coords[..., 0], cmap='viridis', origin='lower', extent=extent)
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-        fig.colorbar(im, cax=cax, orientation='vertical', label=r'$x$ [R$_\odot$]')
-        ax.set_title(r'$x$ [R$_\odot$]')
-
-        ax = axs[1, 1]
-        im = ax.imshow(cartesian_coords[..., 1], cmap='viridis', origin='lower', extent=extent)
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-        fig.colorbar(im, cax=cax, orientation='vertical', label=r'$y$ [R$_\odot$]')
-        ax.set_title(r'$y$ [R$_\odot$]')
-
-        ax = axs[1, 2]
-        im = ax.imshow(cartesian_coords[..., 2], cmap='viridis', origin='lower', extent=extent)
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
-        fig.colorbar(im, cax=cax, orientation='vertical', label=r'$z$ [R$_\odot$]')
-        ax.set_title(r'$z$ [R$_\odot$]')
-
-        for ax in axs.flat:
-            ax.set_xlabel(r'Longitude [deg]')
-            ax.set_ylabel(r'Latitude [deg]')
-        # add subtitle with date
-        plt.suptitle(f'{target_time}', fontsize=16)
-        plt.tight_layout()
-        plt.savefig(os.path.join(out_path, f'xyz_step{i:03d}.jpg'), dpi=300)
-        plt.close()
+        # fig, axs = plt.subplots(2, 3, figsize=(10, 5))
+        #
+        # ax = axs[0, 0]
+        # im = ax.imshow(b_xyz[..., 0], cmap='gray', norm=b_norm, origin='lower', extent=extent)
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+        # fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{r}$ [G]')
+        # ax.set_title(r'$B_\text{x}$ [G]')
+        #
+        # ax = axs[0, 1]
+        # im = ax.imshow(b_xyz[..., 1], cmap='gray', norm=b_norm, origin='lower', extent=extent)
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+        # fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{t}$ [G]')
+        # ax.set_title(r'$B_\text{y}$ [G]')
+        #
+        # ax = axs[0, 2]
+        # im = ax.imshow(b_xyz[..., 2], cmap='gray', norm=b_norm, origin='lower', extent=extent)
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+        # fig.colorbar(im, cax=cax, orientation='vertical', label=r'$B_\text{p}$ [G]')
+        # ax.set_title(r'$B_\text{z}$ [G]')
+        #
+        # ax = axs[1, 0]
+        # im = ax.imshow(cartesian_coords[..., 0], cmap='viridis', origin='lower', extent=extent)
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+        # fig.colorbar(im, cax=cax, orientation='vertical', label=r'$x$ [R$_\odot$]')
+        # ax.set_title(r'$x$ [R$_\odot$]')
+        #
+        # ax = axs[1, 1]
+        # im = ax.imshow(cartesian_coords[..., 1], cmap='viridis', origin='lower', extent=extent)
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+        # fig.colorbar(im, cax=cax, orientation='vertical', label=r'$y$ [R$_\odot$]')
+        # ax.set_title(r'$y$ [R$_\odot$]')
+        #
+        # ax = axs[1, 2]
+        # im = ax.imshow(cartesian_coords[..., 2], cmap='viridis', origin='lower', extent=extent)
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
+        # fig.colorbar(im, cax=cax, orientation='vertical', label=r'$z$ [R$_\odot$]')
+        # ax.set_title(r'$z$ [R$_\odot$]')
+        #
+        # for ax in axs.flat:
+        #     ax.set_xlabel(r'Longitude [deg]')
+        #     ax.set_ylabel(r'Latitude [deg]')
+        # # add subtitle with date
+        # plt.suptitle(f'{target_time}', fontsize=16)
+        # plt.tight_layout()
+        # plt.savefig(os.path.join(out_path, f'xyz_step{i:03d}.jpg'), dpi=300)
+        # plt.close()

pme/loader/spherical.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 from itertools import repeat
 from multiprocessing import Pool
+from typing import Iterable
 
 import numpy as np
 import torch
@@ -47,13 +48,13 @@ class SphericalDataModule(LightningDataModule):
         for i, train_config in enumerate(train_configs):
             ds_type = train_config['type'].lower()
             if ds_type == 'hmi':
-                train_files = self._load_files(train_config['data_path'])
+                train_files = self._load_IQUV_files(train_config['data_path'])
                 ds_class = HMISphericalDataset
             elif ds_type == 'phi-hrt':
-                train_files = sorted(glob.glob(train_config['data_path']))
+                train_files = self._load_all_files(train_config['data_path'])
                 ds_class = PHIHRTSphericalDataset
             elif ds_type == 'phi-fdt':
-                train_files = sorted(glob.glob(train_config['data_path']))
+                train_files = self._load_all_files(train_config['data_path'])
                 ds_class = PHIFDTSphericalDataset
             else:
                 raise ValueError(f'Unknown dataset type: {ds_type}')
@@ -98,11 +99,11 @@ class SphericalDataModule(LightningDataModule):
         for ds in train_datasets[::sample_step]:
             self.plot_dataset(ds)
 
+        ds_normalization = valid_config.get('stokes_normalization', stokes_normalization)
         valid_ds_type = valid_config['type'].lower()
         if valid_ds_type == 'hmi':
-            valid_files = self._load_files(valid_config['data_path'])
+            valid_files = self._load_IQUV_files(valid_config['data_path'])
             sample_idx = valid_config.get('sample_idx', len(valid_files) // 2)
-            ds_normalization = valid_config.get('stokes_normalization', stokes_normalization)
             self.valid_dataset = HMISphericalDataset(valid_files[sample_idx],
                                                      ds_id='valid', instrument_id=valid_config['instrument_id'],
                                                      seconds_per_dt=seconds_per_dt,
@@ -117,7 +118,7 @@ class SphericalDataModule(LightningDataModule):
                                                         ds_id='valid', instrument_id=valid_config['instrument_id'],
                                                         seconds_per_dt=seconds_per_dt,
                                                         Rs_per_ds=Rs_per_ds, ref_time=ref_time,
-                                                        stokes_normalization=stokes_normalization,
+                                                        stokes_normalization=ds_normalization,
                                                         batch_size=self.batch_size, work_directory=work_directory,
                                                         filter_nans=False, shuffle=False)
         elif valid_ds_type == 'phi-fdt':
@@ -127,7 +128,7 @@ class SphericalDataModule(LightningDataModule):
                                                         ds_id='valid', instrument_id=valid_config['instrument_id'],
                                                         seconds_per_dt=seconds_per_dt,
                                                         Rs_per_ds=Rs_per_ds, ref_time=ref_time,
-                                                        stokes_normalization=stokes_normalization,
+                                                        stokes_normalization=ds_normalization,
                                                         batch_size=self.batch_size, work_directory=work_directory,
                                                         filter_nans=False, shuffle=False)
         else:
@@ -173,7 +174,7 @@ class SphericalDataModule(LightningDataModule):
         wandb.log({'Data Overview': wandb.Image(fig)})
         plt.close(fig)
 
-    def _load_files(self, data_path, num_wl=6):
+    def _load_IQUV_files(self, data_path, num_wl=6):
         # load maps
         I = np.stack([sorted(glob.glob(os.path.join(data_path, f'*I{int(i)}.fits')))
                       for i in range(num_wl)], -1)  # t, wl
@@ -185,6 +186,15 @@ class SphericalDataModule(LightningDataModule):
                       for i in range(num_wl)], -1)  # t, wl
         files = np.stack([I, Q, U, V], 1)  # t, stokes, wl
         return files
+
+    def _load_all_files(self, data_path):
+        if isinstance(data_path, str):
+            return sorted(glob.glob(data_path))
+        elif isinstance(data_path, Iterable):
+            files = [f  for d in data_path for f in glob.glob(d)]
+            return sorted(files)
+        else:
+            raise ValueError(f'Unknown data path type: {type(data_path)}. Expected str or Iterable[str].')
 
     def train_dataloader(self):
         # shuffle asynchronously
@@ -285,15 +295,14 @@ def load_v_observer_LOS(s_map):
     hpc_out = hgc_out.transform_to(frame=frames.Helioprojective)
 
     # Components of the satellite velocity
-    v_sdo_r = s_map.meta['OBS_VR']
+    v_sdo_r = s_map.meta['OBS_VR'] # positive is away from Sun
     v_sdo_w = s_map.meta['OBS_VW']
     v_sdo_n = s_map.meta['OBS_VN']
 
     theta_x = hpc_out.Tx.to_value(u.rad)
     theta_y = hpc_out.Ty.to_value(u.rad)
 
-    theta_p = np.arctan2(np.sqrt(np.cos(theta_y) ** 2 * np.sin(theta_x) ** 2 + np.sin(theta_y) ** 2),
-                         np.cos(theta_y) * np.cos(theta_x))
+    theta_p = np.arctan2(np.sqrt(np.cos(theta_y) ** 2 * np.sin(theta_x) ** 2 + np.sin(theta_y) ** 2), np.cos(theta_y) * np.cos(theta_x))
     psi = np.arctan2(-np.cos(theta_y) * np.sin(theta_x), np.sin(theta_y))
 
     # satellite motion
@@ -335,6 +344,10 @@ class PHIHRTSphericalDataset(SphericalDataset):
 
         # set Earth corrected observation time
         header['DATE-OBS'] = header['DATE_EAR']
+
+        # add CROTA2 to header if not present
+        if 'CROTA2' not in header:
+            header['CROTA2'] = header['CROTA']
 
         # (wl, stokes, x, y)
         data = fits.getdata(file)

pme/model.py
@@ -224,11 +224,11 @@ class NormalizationModule(nn.Module):
     def __init__(self, value_range):
         super().__init__()
         self.register_buffer("value_range", torch.tensor(value_range, dtype=torch.float32)[None, :, None, :])
-        self.register_buffer("stretch", torch.tensor(np.arcsinh(1e1), dtype=torch.float32))
+        self.register_buffer("stretch", torch.tensor(np.arcsinh(1e3), dtype=torch.float32))
 
     def forward(self, stokes):
         stokes = stokes / self.value_range[..., 1]  # normalize by max value (I = [0, 1]; QUV = [-1, 1])
-        stokes = torch.asinh(stokes * 1e1) / self.stretch
+        stokes = torch.asinh(stokes * 1e3) / self.stretch
         return stokes
 
 

pme/train/me_spherical_module.py
@@ -151,7 +151,7 @@ class MESphericalModule(LightningModule):
         I_loss, Q_loss, U_loss, V_loss = stokes_loss.mean(dim=0)
 
         # weighted loss - apply lambda weights for each stokes parameter
-        stokes_loss = stokes_loss * self.lambda_stokes[None, :]
+        stokes_loss = (stokes_loss * self.lambda_stokes[None, :]).sum(-1)
 
         #################################################
         # compute physics losses
@@ -206,6 +206,7 @@ class MESphericalModule(LightningModule):
         return {"loss": total_loss,
                 "I_loss": I_loss, "Q_loss": Q_loss,
                 "U_loss": U_loss, "V_loss": V_loss,
+                "stokes_loss": stokes_loss,
                 "induction_loss": physics_losses['induction'].mean(),
                 "divergence_loss": physics_losses['divergence'].mean(),
                 "force_free_loss": physics_losses['force_free'].mean(),
@@ -290,8 +291,7 @@ class MESphericalModule(LightningModule):
 
     def scale_parameters(self, output, transformed_output, v_obs_los):
         v_dop = transformed_output['v_dop'] * self.meters_per_ds / self.seconds_per_dt
-        v_dop = v_dop - v_obs_los  # add doppler correction - spacecraft velocity
-
+        v_dop = v_dop + v_obs_los  # add doppler correction - spacecraft velocity
 
         forward_params = {'b_field': transformed_output['b_field'] * self.gauss_per_dB,
                           'sin_inc2': transformed_output['sin_inc2'],
@@ -342,7 +342,7 @@ class MESphericalModule(LightningModule):
         # transform V
         v_xyz = torch.cat([output['v_x'], output['v_y'], output['v_z']], dim=-1)
         v_rtp = torch.einsum("...ij,...j->...i", cartesian_to_spherical_transform, v_xyz)
-        v_rtp_alt = torch.stack([v_rtp[..., 0], v_rtp[..., 1], v_rtp[..., 2] + v_rot], dim=-1)
+        v_rtp_alt = torch.stack([v_rtp[..., 0], v_rtp[..., 1], v_rtp[..., 2] - v_rot], dim=-1)
         v_img = torch.einsum("...ij,...j->...i", rtp_to_img_transform, v_rtp_alt)
 
         v_dop = v_img[..., 2:3]
@@ -739,55 +739,56 @@ class MESphericalModule(LightningModule):
     def plot_v_rtp(self, parameters):
         v_rtp = parameters['v_rtp']
         v_img = parameters['v_img']
+        v_cmap = 'seismic'
 
         v_min_max = max(np.nanmax(np.abs(v_img)), np.nanmax(np.abs(v_rtp)))
         norm = Normalize(vmin=-v_min_max, vmax=v_min_max)
 
         fig, axs = plt.subplots(3, 3, figsize=(10, 7), dpi=150)
         ax = axs[0, 0]
-        im = ax.imshow(v_rtp[..., 0], cmap='seismic_r', origin='lower', norm=norm)
+        im = ax.imshow(v_rtp[..., 0], cmap=v_cmap, origin='lower', norm=norm)
         ax.set_title("$v_r$")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
 
         ax = axs[0, 1]
-        im = ax.imshow(v_rtp[..., 1], cmap='seismic_r', origin='lower', norm=norm)
+        im = ax.imshow(v_rtp[..., 1], cmap=v_cmap, origin='lower', norm=norm)
         ax.set_title("$v_t$")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
 
         ax = axs[0, 2]
-        im = ax.imshow(v_rtp[..., 2], cmap='seismic_r', origin='lower', norm=norm)
+        im = ax.imshow(v_rtp[..., 2], cmap=v_cmap, origin='lower', norm=norm)
         ax.set_title("$v_p$")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
 
         ax = axs[1, 0]
-        im = ax.imshow(v_img[..., 0], cmap='seismic_r', origin='lower', norm=norm)
+        im = ax.imshow(v_img[..., 0], cmap=v_cmap, origin='lower', norm=norm)
         ax.set_title(r"$v_\text{xi}$")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
 
         ax = axs[1, 1]
-        im = ax.imshow(v_img[..., 1], cmap='seismic_r', origin='lower', norm=norm)
+        im = ax.imshow(v_img[..., 1], cmap=v_cmap, origin='lower', norm=norm)
         ax.set_title(r"$v_\text{eta}$")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
 
         ax = axs[1, 2]
-        im = ax.imshow(v_img[..., 2], cmap='seismic_r', origin='lower', norm=norm)
+        im = ax.imshow(v_img[..., 2], cmap=v_cmap, origin='lower', norm=norm)
         ax.set_title(r"$v_\text{zeta}$")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(im, cax=cax)
 
         ax = axs[2, 0]
-        im = ax.imshow(parameters['v_obs_los'], cmap='seismic_r', origin='lower', norm=norm)
+        im = ax.imshow(parameters['v_obs_los'], cmap=v_cmap, origin='lower', norm=norm)
         ax.set_title("V_obs_los")
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)