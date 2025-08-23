fix spheromak test set generation, add missing test set transforms, update test set plots, update spheromak parameters, fix invalid velocity scaling of spheromak, remove redundant tensor transforms, fix create spheromak bug, fix mu for test set, add rotational velocity to spherical test set

pme/data/create_cartesian_test_set.py
@@ -3,9 +3,10 @@ import os
 from multiprocessing import Pool
 
 import numpy as np
+import torch
 from astropy import units as u
 from matplotlib import pyplot as plt
-from matplotlib.colors import LogNorm
+from matplotlib.colors import LogNorm, Normalize
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from sunpy.coordinates import frames
 from sunpy.map import Map, all_coordinates_from_map
@@ -25,28 +26,28 @@ def plot_stokes(profile, save_path):
     fig, axs = plt.subplots(1, 4, figsize=(16, 4))
 
     ax = axs[0]
-    im = ax.imshow(profile[..., 0, :].sum(axis=-1), norm=LogNorm())
+    im = ax.imshow(profile[..., 0, :].sum(axis=-1), norm=Normalize())
     ax.set_title("I")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     plt.colorbar(im, cax=cax)
 
     ax = axs[1]
-    im = ax.imshow(profile[..., 1, :].sum(axis=-1), norm=LogNorm())
+    im = ax.imshow(profile[..., 1, :].sum(axis=-1), norm=Normalize())
     ax.set_title("Q")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     plt.colorbar(im, cax=cax)
 
     ax = axs[2]
-    im = ax.imshow(profile[..., 2, :].sum(axis=-1), norm=LogNorm())
+    im = ax.imshow(profile[..., 2, :].sum(axis=-1), norm=Normalize())
     ax.set_title("U")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     plt.colorbar(im, cax=cax)
 
     ax = axs[3]
-    im = ax.imshow(profile[..., 3, :].sum(axis=-1), norm=LogNorm())
+    im = ax.imshow(profile[..., 3, :].sum(axis=-1), norm=Normalize())
     ax.set_title("V")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
@@ -75,14 +76,14 @@ def plot_parameters(parameters, save_path):
     plt.colorbar(im, cax=cax)
 
     ax = axs[0, 1]
-    im = ax.imshow(parameters['inc'], cmap='RdBu_r')
+    im = ax.imshow(np.arccos(parameters['cos_inc']), cmap='RdBu_r')
     ax.set_title("Inclination")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)
     plt.colorbar(im, cax=cax)
 
     ax = axs[0, 2]
-    im = ax.imshow(parameters['azi'], cmap='twilight')
+    im = ax.imshow(parameters['azi'] % (2 * np.pi), cmap='twilight', vmin=0, vmax=2 * np.pi)
     ax.set_title("Azimuth")
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='5%', pad=0.05)

pme/data/create_spheromak_test_set.py
@@ -1,7 +1,4 @@
-import numpy as np
 # Constants
-from scipy.special import jvp
-
 import argparse
 import glob
 import os
@@ -11,70 +8,52 @@ from multiprocessing import Pool
 
 import numpy as np
 import pandas as pd
-from sunpy.coordinates import get_earth
 import torch
 from astropy import units as u
 from astropy.coordinates import SkyCoord, Angle
+from scipy.special import jvp
 from sunpy.coordinates import frames
 from sunpy.map import make_heliographic_header, Map, make_fitswcs_header, all_coordinates_from_map
 from sunpy.sun import constants
 
 from pme.data.create_cartesian_test_set import plot_parameters, plot_stokes, plot_brtp, plot_coords
+from pme.data.differential_rotation import carrington_rotation_velocity
 from pme.data.test_set_generator import TestSetGenerator, load_parameters, load_fits_profiles
-from pme.data.util import image_to_spherical_matrix
-from pme.data.util import solar_differential_rotation_velocity
+from pme.data.util import image_to_spherical_matrix, vector_spherical_to_cartesian, vector_cartesian_to_spherical
 
-import matplotlib.pyplot as pl
-
-# from pme.convert.vtk import save_vtk
 
 class SpheromakTestSetGenerator(TestSetGenerator):
 
-    def _create_spheromak_params(self, time_step, t_start = 0, t_end =600):
-        '''
-        Create the map of parameters for the spheromak test case on a sphere
+    def __init__(self, ref_time, carrington_map_resolution=(180, 360), *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ref_time = ref_time
+        self.carrington_map_resolution = carrington_map_resolution
 
-        '''
+    def _create_spheromak_params(self, time_seconds):
         # Initial conditions
-
         t0 = 0
+        R_solar = 1
+        meters_per_Rs = (1 * u.Rsun).to_value(u.m)
 
-        R_solar = 1 # 699 Mm
-
-        time_step = 0
-        num_longitude_points = self.nx
-        num_latitude_points = self.ny
-
-        dlongitude = 2 * np.pi / num_longitude_points
-        dlatitude = np.pi / num_latitude_points
-
-        # Spatial parameters
-        R_0 = 2
-
-        coords_polar = np.stack(np.meshgrid(R_solar,
-                                            np.arange(-np.pi / 2, np.pi / 2, dlatitude),
-                                            np.arange(-np.pi, np.pi, dlongitude),
-                                            time_step, indexing='ij'), -1)
-        # print(f"timestep is {time_step}")
-        #
-        #  breakpoint()
-        R, THETA, PHI, T = (coords_polar[..., 0],
-                            coords_polar[..., 1] + np.pi/2,
-                            coords_polar[..., 2],
-                            coords_polar[..., 3])
+        coords_spherical = np.stack(np.meshgrid(R_solar,
+                                                np.linspace(0, np.pi, self.carrington_map_resolution[0]),
+                                                np.linspace(0, 2 * np.pi, self.carrington_map_resolution[1]),
+                                                time_seconds, indexing='ij'), -1)
+        R, THETA, PHI, T = (coords_spherical[..., 0],
+                            coords_spherical[..., 1],
+                            coords_spherical[..., 2],
+                            coords_spherical[..., 3])
 
         # Calculate the spheromak solution
 
+        # Spatial parameters
+        R_0 = 0.5
         B_0 = 2000
-        n = 2
-        m = 3
-        gamma = 2.5e5  # 2.5e5 # Mm/s
-        C_alpha = 1.4934
+        n = 1
+        m = 0
+        gamma = (0.1 * u.km / u.s).to_value(u.Rsun / u.s)
+        C_alpha = 4.4934
         alpha_0 = C_alpha / R_0
-        #
-        A_r = np.zeros_like(R)
-        A_theta = 0
-        A_phi = 0
         #
         alpha = C_alpha * (R_0 + gamma * (T - t0) ** n) ** -1
         dalpha_dt = - C_alpha * (R_0 + gamma * (T - t0) ** n) ** -2 * gamma * n * (T - t0) ** (n - 1)
@@ -91,36 +70,40 @@ class SpheromakTestSetGenerator(TestSetGenerator):
         E_phi = B_theta * dalpha_dt / alpha * R
         #
         # c = np.stack([R, THETA, PHI], -1)
-        B = np.stack([B_r, B_theta, B_phi], -1)
-        E = np.stack([E_r, E_theta, E_phi], -1)
+        B_rtp = np.stack([B_r, B_theta, B_phi], -1)
+        E_rtp = np.stack([E_r, E_theta, E_phi], -1)
 
-        V = np.divide(np.cross(E, B), np.array(B) * np.array(B).sum(-1, keepdims=True))
+        B = vector_spherical_to_cartesian(B_rtp, coords_spherical)
+        E = vector_spherical_to_cartesian(E_rtp, coords_spherical)
+        V = np.divide(np.cross(E, B), (B ** 2).sum(-1, keepdims=True))
 
-        b_r_arr = torch.tensor(B_r.squeeze(), dtype=torch.float32)
-        b_theta_arr = torch.tensor(B_theta.squeeze(), dtype=torch.float32)
-        b_phi_arr = torch.tensor(B_phi.squeeze(), dtype=torch.float32)
-        b0_arr = self.b1 * torch.ones_like(b_r_arr)
-        b1_arr = self.b0 * torch.ones_like(b_r_arr)
-        vmac_arr = self.vmac * torch.ones_like(b_r_arr)
-        v_r_arr = torch.tensor(V[..., 0].squeeze(), dtype=torch.float32)
-        v_theta_arr = torch.tensor(V[..., 1].squeeze(), dtype=torch.float32)
-        v_phi_arr = torch.tensor(V[..., 2].squeeze(), dtype=torch.float32)
-        damping_arr = self.damping * torch.ones_like(b_r_arr)
-        mu_arr = self.mu * torch.ones_like(b_r_arr)
-        vdop_arr = self.vdop * torch.ones_like(b_r_arr)
-        kl_arr = self.kl * torch.ones_like(b_r_arr)
+        V_rtp = vector_cartesian_to_spherical(V, coords_spherical)
+        V_r = V_rtp[..., 0] * meters_per_Rs
+        V_theta = V_rtp[..., 1] * meters_per_Rs
+        V_phi = V_rtp[..., 2] * meters_per_Rs
 
-        return {'b0': b0_arr , 'b1': b1_arr, 'b_r': b_r_arr, 'b_theta': b_theta_arr,
+        # reshape for output
+        b_r_arr = B_r[0, :, :, 0]  # squeeze out r and t dimensions
+        b_theta_arr = B_theta[0, :, :, 0]
+        b_phi_arr = B_phi[0, :, :, 0]
+
+        v_r_arr = V_r[0, :, :, 0]
+        v_theta_arr = V_theta[0, :, :, 0]
+        v_phi_arr = V_phi[0, :, :, 0]
+
+        damping_arr = self.damping * np.ones_like(b_r_arr)
+        mu_arr = self.mu * np.ones_like(b_r_arr)
+        kl_arr = self.kl * np.ones_like(b_r_arr)
+        b0_arr = self.b1 * np.ones_like(b_r_arr)
+        b1_arr = self.b0 * np.ones_like(b_r_arr)
+        vmac_arr = self.vmac * np.ones_like(b_r_arr)
+
+        return {'b0': b0_arr, 'b1': b1_arr, 'b_r': b_r_arr, 'b_theta': b_theta_arr,
                 'damping': damping_arr, 'kl': kl_arr, 'mu': mu_arr, 'b_phi': b_phi_arr,
-                'v_r': v_r_arr, 'v_theta': v_theta_arr, 'v_phi':v_phi_arr,
+                'v_r': v_r_arr, 'v_theta': v_theta_arr, 'v_phi': v_phi_arr,
                 'vmac': vmac_arr}
 
     def _transform_parameters(self, input_parameters, obs_coord):
-
-        latitudes = np.linspace(-np.pi / 2, np.pi / 2, self.ny) * u.rad
-        # v_diff = solar_differential_rotation_velocity(latitudes).to_value(u.m / u.s)
-        # input_parameters['v_phi'] += v_diff[:, None]
-
         # create carrington map header
         carrington_header = make_heliographic_header(obs_coord.obstime, 'earth',
                                                      input_parameters['b_r'].shape,
@@ -129,103 +112,105 @@ class SpheromakTestSetGenerator(TestSetGenerator):
         # create helioprojective map header
         solar_semidiameter_rad = np.arcsin(constants.radius / obs_coord.radius)
         angular_radius = Angle(solar_semidiameter_rad.to(u.arcsec))
-
-        scale = angular_radius.to(u.arcsec) / (self.nx // 2 * u.pix), angular_radius.to(u.arcsec) / (
-                self.ny // 2 * u.pix)
+        scale = (angular_radius.to(u.arcsec) / (self.nx // 2 * u.pix),
+                 angular_radius.to(u.arcsec) / (self.ny // 2 * u.pix))
         dummy_data = np.zeros((self.nx, self.ny), dtype=np.float32)
         reference_coord = SkyCoord(0 * u.arcsec, 0 * u.arcsec, observer=obs_coord, frame=frames.Helioprojective)
         helioprojective_header = make_fitswcs_header(dummy_data, reference_coord, scale=u.Quantity(scale))
-        # transform all parameters to helioprojective frame
-        exclude_parameters = ['b_r', 'b_theta', 'b_azi', 'v_r', 'v_phi', 'v_theta']
-        # stack b vector
-        b_r = input_parameters.pop('b_r')
-        b_theta = input_parameters.pop('b_theta')
-        b_phi = input_parameters.pop('b_phi')
-        b_rtp = np.stack([b_r, b_theta, b_phi], -1)
 
-        v_r = input_parameters.pop('v_r')
-        v_theta = input_parameters.pop('v_theta')
-        v_phi = input_parameters.pop('v_phi')
-        v_rtp = np.stack([v_r, v_theta, v_phi], -1)
-
-        input_parameters = {k: v for k, v in input_parameters.items() if k not in exclude_parameters}
         transformed_parameters = {}
         for k, parameter in input_parameters.items():
-
             carrington_map = Map(np.array(parameter), carrington_header)
             helioprojective_map = carrington_map.reproject_to(helioprojective_header)
             transformed_parameters[k] = helioprojective_map.data
-        print(f"transformed_parameters: {transformed_parameters['vmac'][200:210, 200:210]}")
-
 
         # create dummy helioprojective map
-
         dummy_carrington = np.zeros_like(input_parameters['kl'])
         carrington_map = Map(dummy_carrington, carrington_header)
         helioprojective_map = carrington_map.reproject_to(helioprojective_header)
+
         # Compute mu for the helioprojective map
-        helioprojective_coords = all_coordinates_from_map(helioprojective_map)
-        helioprojective_coords = helioprojective_coords.transform_to(frames.Helioprojective)
+        map_coords = all_coordinates_from_map(helioprojective_map)
+        projective_coords = map_coords.transform_to(frames.Helioprojective)
         radial_distance = np.sqrt(
-            helioprojective_coords.Tx ** 2 + helioprojective_coords.Ty ** 2) / helioprojective_map.rsun_obs
-        mu = np.cos(radial_distance.to_value(u.dimensionless_unscaled) * np.pi / 2)
+            projective_coords.Tx ** 2 +
+            projective_coords.Ty ** 2) / helioprojective_map.rsun_obs
+        mu = np.sqrt(1 - radial_distance.to_value() ** 2)
         mu = mu.astype(np.float32)
-        mu[mu < 0] = np.nan
         transformed_parameters['mu'] = mu
+
+        # Convert B and V
+        b_r = transformed_parameters.pop('b_r')
+        b_theta = transformed_parameters.pop('b_theta')
+        b_phi = transformed_parameters.pop('b_phi')
+        b_rtp = np.stack([b_r, b_theta, b_phi], -1)
+
+        v_r = transformed_parameters.pop('v_r')
+        v_theta = transformed_parameters.pop('v_theta')
+        v_phi = transformed_parameters.pop('v_phi')
+        v_rtp = np.stack([v_r, v_theta, v_phi], -1)
 
         # Transform vector quantities -- B, V -- to image coordinates
         # create transformation matrix
-        carrington_coords = helioprojective_coords.transform_to(frames.HeliographicCarrington)
+        carrington_coords = map_coords.transform_to(frames.HeliographicCarrington)
         lat, lon = carrington_coords.lat.to_value(u.rad), carrington_coords.lon.to_value(u.rad)
         latc, lonc = helioprojective_map.carrington_latitude.to_value(
             u.rad), helioprojective_map.carrington_longitude.to_value(u.rad)
 
-        # TODO check that CRLT_OBS is in rad units as provided by the map
         pAng = -np.deg2rad(helioprojective_map.meta.get('CROTA2', 0))
         a_matrix = image_to_spherical_matrix(lon, lat, lonc, latc, pAng=pAng)
         rtp_to_img_transform = np.linalg.inv(a_matrix)
 
-
         # transform b vector to image frame
         b_img = np.einsum("...ij,...j->...i", rtp_to_img_transform, b_rtp)  # in image xyz
         # b_im = (xi, eta, zeta)
-        # convert to ME parameters
-        b_field = np.linalg.norm(b_img, axis=-1)
-        b_inc = np.arccos(b_img[..., 2] / (b_field + 1e-8))
-        b_azi = np.arctan2(-b_img[..., 0], b_img[..., 1])
 
-        # stack v vector
+        # convert to ME parameters
+        # FLD
+        b_field = np.linalg.norm(b_img, axis=-1)
+        # INC
+        sin_inc2 = (b_img[..., 0] ** 2 + b_img[..., 1] ** 2) / (b_field ** 2 + 1e-8)
+        cos_inc = b_img[..., 2] / (b_field + 1e-8)
+        # AZI
+        sin2azi = -2 * b_img[..., 0] * b_img[..., 1] / (b_img[..., 0] ** 2 + b_img[..., 1] ** 2 + 1e-8)
+        cos2azi = -(b_img[..., 0] ** 2 - b_img[..., 1] ** 2) / (b_img[..., 0] ** 2 + b_img[..., 1] ** 2 + 1e-8)
+        azi = np.arctan2(-b_img[..., 0:1], b_img[..., 1:2])
+
+        # add rotation of carrington frame
+        v_rot = carrington_rotation_velocity(lat, radius=(1 * u.Rsun).to_value(u.m), f=np)
+        v_rtp[..., 2] += v_rot  # add rotation in phi direction
 
         # transform b vector to image frame
         v_img = np.einsum("...ij,...j->...i", rtp_to_img_transform, v_rtp)  # in image xyz
         # convert to ME parameters
-        vdop = v_img[..., 2]
+        vdop = -v_img[..., 2]
 
         transformed_parameters['b_field'] = b_field
-        transformed_parameters['inc'] = b_inc
-        transformed_parameters['azi'] = b_azi
-
+        transformed_parameters['sin_inc2'] = sin_inc2
+        transformed_parameters['cos_inc'] = cos_inc
+        transformed_parameters['sin2azi'] = sin2azi
+        transformed_parameters['cos2azi'] = cos2azi
+        transformed_parameters['azi'] = azi
         transformed_parameters['vdop'] = vdop
 
         transformed_parameters['b_rtp'] = b_rtp
         transformed_parameters['v_rtp'] = v_rtp
-        # breakpoint()
-        # print(f"transformed_parameters_vmac: {transformed_parameters['vmac']}")
+
         return transformed_parameters, helioprojective_map
 
-    def create_spheromak_time_step(self, time_step, obs_coord, resolution=(180, 180)):
-
-        parameters = self._create_spheromak_params(time_step, resolution)
+    def create_spheromak_time_step(self, time_seconds, obs_coord):
+        parameters = self._create_spheromak_params(time_seconds)
         # convert to numpy arrays
         transformed_parameters, dummy_helioprojective_map = self._transform_parameters(parameters, obs_coord)
         # convert back to torch tensors
         transformed_parameters = {k: torch.tensor(v, dtype=torch.float32) for k, v in transformed_parameters.items()}
-        input_parameters = {k: v for k, v in transformed_parameters.items() if k not in ['b_rtp', 'v_rtp']}
+        input_parameters = {k: v for k, v in transformed_parameters.items() if k not in ['b_rtp', 'v_rtp', 'azi']}
         stokes_profiles = self.convert_to_profiles(**input_parameters)
         return stokes_profiles, transformed_parameters, dummy_helioprojective_map
 
     def create_spherical_time_step_file(self, t_step, base_path, obs_coords):
-        profiles, parameters, dummy_map = self.create_spheromak_time_step(t_step, obs_coords)
+        time_seconds = (obs_coords.obstime.to_datetime() - self.ref_time).total_seconds()
+        profiles, parameters, dummy_map = self.create_spheromak_time_step(time_seconds, obs_coords)
         header = dummy_map.meta
         header['OBS_VR'] = 0
         header['OBS_VW'] = 0
@@ -239,57 +224,35 @@ class SpheromakTestSetGenerator(TestSetGenerator):
         np.savez(os.path.join(base_path, f'parameters_{t_step:03d}.npz'), **parameters)
 
 
-def vector_spherical_to_cartesian(v, c):
-    vr, vt, vp = v[..., 0], v[..., 1], v[..., 2]
-    r, t, p = c[..., 0], c[..., 1], c[..., 2]
-    sin = np.sin
-    cos = np.cos
-    #
-    vx = vr * sin(t) * cos(p) + vt * cos(t) * cos(p) - vp * sin(p)
-    vy = vr * sin(t) * sin(p) + vt * cos(t) * sin(p) + vp * cos(p)
-    vz = vr * cos(t) - vt * sin(t)
-    #
-    return np.stack([vx, vy, vz], -1)
-
-def to_spherical(v):
-    x, y, z = v[..., 0], v[..., 1], v[..., 2]
-    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
-    theta = np.arccos(z / r)
-    phi = np.arctan2(y, x)
-    return np.stack([r, theta, phi], -1)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--out_path', type=str, required=True,
-                        help='base path for the output data')
-    parser.add_argument('--resolution', type=int, nargs=2, default=[256, 256],
-                        help='resolution of the images')
-    parser.add_argument('--n_time_steps', type=int, default=100,
-                        help='number of time steps to generate')
-
+    parser.add_argument('--out_path', type=str, required=True, help='base path for the output data')
+    parser.add_argument('--resolution', type=int, nargs=2, default=[256, 256], help='resolution of the images')
+    parser.add_argument('--n_time_steps', type=int, default=100, help='number of time steps to generate')
+    parser.add_argument('--obs_lon', type=float, default=0.0, help='observer longitude in degrees')
+    parser.add_argument('--obs_lat', type=float, default=0.0, help='observer latitude in degrees')
     args = parser.parse_args()
 
     out_path = args.out_path
 
     os.makedirs(out_path, exist_ok=True)
 
-    n_proc = 30
+    n_proc = 16
 
-    obs_lon = 0 * u.deg
-    obs_lat = 0 * u.deg
+    obs_lon = args.obs_lon * u.deg
+    obs_lat = args.obs_lat * u.deg
     observer_distance = 1 * u.AU
 
     t_start = datetime(2025, 1, 1, )
     t_end = datetime(2025, 2, 1)
     t_range = pd.date_range(t_start, t_end, periods=args.n_time_steps)
 
-    lambda_grid = np.array([-0.1695, -0.1017, -0.0339, +0.0339, +0.1017, +0.1695]) * u.AA  # From Phillip Scherrer
-    lambda0 = 617.33433 * u.nm  # From Phillip Scherrer
+    lambda0 = 6173.3433 * u.AA
+    lambda_grid = np.array([-0.1695, -0.1017, -0.0339, +0.0339, +0.1017, +0.1695]) * u.AA
 
     data_generator = SpheromakTestSetGenerator(nx=args.resolution[0], ny=args.resolution[1],
                                                lambda0=lambda0, lambda_grid=lambda_grid,
-                                               g_up=2.50)
+                                               g_up=2.50, ref_time=t_start)
 
     observers = []
 

pme/data/test_set_generator.py
@@ -3,6 +3,7 @@ import os.path
 
 import numpy as np
 import torch
+from astropy import units as u
 from astropy.io import fits
 
 from pme.train.me_atmosphere import MEAtmosphere
@@ -22,10 +23,10 @@ def convert_rt_to_xy(r, t):
     return int(x), int(y)
 
 
-class TestSetGenerator():
+class TestSetGenerator:
 
     def __init__(self, lambda0, lambda_grid,
-                 j_up=1.0, j_low=0.0, g_up=2.49, g_low=0,
+                 j_up=1.0, j_low=0.0, g_up=2.5, g_low=0,
                  nx=400, ny=400,
                  b_field_0=2000.0, vmac=2.0 * 1e3, damping=0.2, b0=0.8, b1=0.2, mu=1.0, vdop=2.0 * 1e3, kl=25.0):
         self.lambda0 = lambda0
@@ -56,9 +57,13 @@ class TestSetGenerator():
         return {'stokes_profiles': stokes_profiles}, parameters
 
     def convert_to_profiles(self, b0, b1, b_field, cos2azi, sin2azi, sin_inc2, cos_inc, damping, kl, mu, vdop, vmac):
-        atmos = MEAtmosphere(self.lambda0, self.jUp, self.jLow, self.gUp, self.gLow, self.lambda_grid)
+        atmos = MEAtmosphere(self.lambda0, self.jUp, self.jLow, self.gUp, self.gLow)
         # flatten and forward
-        I, Q, U, V = atmos.forward(b_field.reshape(-1, 1),
+        b_field = b_field.reshape(-1, 1)  # used as reference
+        lambda_grid = torch.tensor(self.lambda_grid.to_value(u.m), dtype=torch.float32, device=b_field.device)
+        lambda_grid = torch.ones_like(b_field) * lambda_grid[None, :]
+        I, Q, U, V = atmos.forward(lambda_grid,
+                                   b_field,
                                    cos2azi.reshape(-1, 1), sin2azi.reshape(-1, 1),
                                    sin_inc2.reshape(-1, 1), cos_inc.reshape(-1, 1),
                                    vmac.reshape(-1, 1), damping.reshape(-1, 1),
@@ -66,7 +71,7 @@ class TestSetGenerator():
                                    vdop.reshape(-1, 1), kl.reshape(-1, 1))
         stokes_profiles = torch.stack([I, Q, U, V], -2).cpu().numpy()
         # (x, y, n_lambda, n_stokes)
-        stokes_profiles = stokes_profiles.reshape(*b_field.shape, 4, *self.lambda_grid.shape)
+        stokes_profiles = stokes_profiles.reshape(*cos2azi.shape, 4, *self.lambda_grid.shape)
         return stokes_profiles
 
     def _load_parameters(self, time_step, resolution=None):

pme/data/util.py
@@ -1,8 +1,6 @@
 import numpy as np
 import torch
 from astropy import units as u
-from astropy.constants import R_sun
-# Some documentation to be included
 
 def spherical_to_cartesian_matrix(c):
     r, t, p = c[..., 0], c[..., 1], c[..., 2]
@@ -28,6 +26,32 @@ def cartesian_to_spherical_matrix(c):
     matrix = np.stack(matrix, axis=-1).reshape((*c.shape[:-1], 3, 3))
     #
     return matrix
+
+def vector_spherical_to_cartesian(v, c, f=np):
+    vr, vt, vp = v[..., 0], v[..., 1], v[..., 2]
+    r, t, p = c[..., 0], c[..., 1], c[..., 2]
+    sin = f.sin
+    cos = f.cos
+    #
+    vx = vr * sin(t) * cos(p) + vt * cos(t) * cos(p) - vp * sin(p)
+    vy = vr * sin(t) * sin(p) + vt * cos(t) * sin(p) + vp * cos(p)
+    vz = vr * cos(t) - vt * sin(t)
+    #
+    return f.stack([vx, vy, vz], -1)
+
+
+def vector_cartesian_to_spherical(v, c, f=np):
+    vx, vy, vz = v[..., 0], v[..., 1], v[..., 2]
+    r, t, p = c[..., 0], c[..., 1], c[..., 2]
+    sin = f.sin
+    cos = f.cos
+    #
+    vr = vx * sin(t) * cos(p) + vy * sin(t) * sin(p) + vz * cos(t)
+    vt = vx * cos(t) * cos(p) + vy * cos(t) * sin(p) - vz * sin(t)
+    vp = - vx * sin(p) + vy * cos(p)
+    #
+    return f.stack([vr, vt, vp], -1)
+
 
 
 def spherical_to_cartesian(v, f=np):
@@ -98,5 +122,5 @@ def solar_differential_rotation_velocity(latitude):
     omega = omega.to(u.rad / u.s)  # Convert to rad/s
 
     # Tangential velocity: v = R * omega * cos(latitude)
-    v = R_sun * omega * np.cos(theta) / u.rad
+    v = (1 * u.Rsun).to_value(u.m) * omega * np.cos(theta) / u.rad
     return v.to(u.m / u.s)